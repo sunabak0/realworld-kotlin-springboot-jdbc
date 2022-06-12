3135a8f7db8f5d9b26a3186926f36ca4162fe2e9
Msksgm <msksgm3124@gmail.com>
2022-06-12 12:10:49 +0900

    first commit

---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplication.kt
@@ -0,0 +1,11 @@
+package com.example.realworldkotlinspringbootjdbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealworldKotlinSpringbootJdbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<RealworldKotlinSpringbootJdbcApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.realworldkotlinspringbootjdbc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealworldKotlinSpringbootJdbcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
