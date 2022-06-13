dc095d568c4bc60c0e6bbead4d7e5d13cd6280ef
sunakan <sunakan03@gmail.com>
2022-06-13 13:36:18 +0900

    SpringBoot起動が必要なテストをコメントアウト

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplicationTests.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplicationTests.kt
@@ -3,10 +3,9 @@ package com.example.realworldkotlinspringbootjdbc
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class RealworldKotlinSpringbootJdbcApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-}
+//@SpringBootTest
+//class RealworldKotlinSpringbootJdbcApplicationTests {
+//    @Test
+//    fun contextLoads() {
+//    }
+//}
