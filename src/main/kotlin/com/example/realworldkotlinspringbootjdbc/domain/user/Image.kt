a6c624150fd5af4ef0f1d5c8475023018e2ff42e
sunakan <sunakan03@gmail.com>
2022-06-13 14:59:17 +0900

    imageを追加

---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/user/Image.kt
@@ -0,0 +1,57 @@
+package com.example.realworldkotlinspringbootjdbc.domain.user
+
+import arrow.core.Option
+import arrow.core.Validated
+import com.example.realworldkotlinspringbootjdbc.util.MyError
+
+interface Image {
+    val value: String
+    data class ValidationErrors(override val errors: List<ValidationError>) : MyError.ValidationErrors<ValidationError>
+    sealed interface ValidationError : MyError.ValidationError {
+        object Required : ValidationError {
+            override val message: String get() = "必須項目です"
+            fun check(image: String?): Validated<Required, String> =
+                Option.fromNullable(image).fold(
+                    { Validated.Invalid(Required) },
+                    { Validated.Valid(it) }
+                )
+        }
+
+        data class TooLong(val image: String) : ValidationError {
+            companion object {
+                private const val maximum: Int = 512
+                fun check(image: String): Validated<TooLong, String> =
+                    if (image.length <= maximum) {
+                        Validated.Valid(image)
+                    } else {
+                        Validated.Invalid(TooLong(image))
+                    }
+            }
+
+            override val message: String get() = "${maximum}文字以下にしてください"
+        }
+    }
+
+    companion object {
+        fun new(image: String?): Validated<ValidationErrors, Image> {
+            val existedImage = when (val it = ValidationError.Required.check(image)) {
+                is Validated.Invalid -> { return Validated.Invalid(ValidationErrors(listOf(it.value))) }
+                is Validated.Valid -> it.value
+            }
+            val errors = mutableListOf<ValidationError>()
+            when (val it = ValidationError.TooLong.check(existedImage)) {
+                is Validated.Invalid -> { errors.add(it.value) }
+                is Validated.Valid -> {}
+            }
+            return if (errors.size == 0) { Validated.Valid(ImageImpl(existedImage)) } else {
+                Validated.Invalid(
+                    ValidationErrors(
+                        errors
+                    )
+                )
+            }
+        }
+    }
+
+    private data class ImageImpl(override val value: String) : Image
+}
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/user/ImageTest.kt
@@ -0,0 +1,28 @@
+package com.example.realworldkotlinspringbootjdbc.domain.user
+
+import arrow.core.Validated
+import net.jqwik.api.ForAll
+import net.jqwik.api.Property
+import net.jqwik.api.constraints.StringLength
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ImageTest {
+    @Test
+    fun `Userのimageは必須項目`() {
+        val invalid = Image.ValidationError.Required.check(null)
+        val valid = Image.ValidationError.Required.check("")
+        Assertions.assertThat(invalid).isEqualTo(Validated.Invalid(Image.ValidationError.Required))
+        Assertions.assertThat(valid).isEqualTo(Validated.Valid(""))
+    }
+    @Property(tries = 100)
+    fun `Userのimageは512文字以下`(
+        @ForAll @StringLength(min = 513) invalidImage: String,
+        @ForAll @StringLength(max = 512) validImage: String,
+    ) {
+        val invalid = Image.ValidationError.TooLong.check(invalidImage)
+        val valid = Image.ValidationError.TooLong.check(validImage)
+        Assertions.assertThat(invalid).isEqualTo(Validated.Invalid(Image.ValidationError.TooLong(invalidImage)))
+        Assertions.assertThat(valid).isEqualTo(Validated.Valid(validImage))
+    }
+}
