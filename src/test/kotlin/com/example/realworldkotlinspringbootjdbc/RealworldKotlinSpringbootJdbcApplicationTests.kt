e6c5cdaf2faa326d25e328ae0996815f1d2e0b09
sunakan <sunakan03@gmail.com>
2022-06-13 15:02:36 +0900

    make fmt

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplicationTests.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplicationTests.kt
@@ -1,11 +1,8 @@
 package com.example.realworldkotlinspringbootjdbc
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-//@SpringBootTest
-//class RealworldKotlinSpringbootJdbcApplicationTests {
+// @SpringBootTest
+// class RealworldKotlinSpringbootJdbcApplicationTests {
 //    @Test
 //    fun contextLoads() {
 //    }
-//}
+// }
