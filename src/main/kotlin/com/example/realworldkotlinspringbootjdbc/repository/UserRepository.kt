1a59822986780330a18879f42dac3fcefafa66d1
sunakan <sunakan03@gmail.com>
2022-06-14 15:48:10 +0900

    UserRepositoryを追加

---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/repository/UserRepository.kt
@@ -0,0 +1,54 @@
+package com.example.realworldkotlinspringbootjdbc.repository
+
+import arrow.core.Either
+import com.example.realworldkotlinspringbootjdbc.domain.RegisteredUser
+import com.example.realworldkotlinspringbootjdbc.domain.UnregisteredUser
+import com.example.realworldkotlinspringbootjdbc.domain.user.Bio
+import com.example.realworldkotlinspringbootjdbc.domain.user.Email
+import com.example.realworldkotlinspringbootjdbc.domain.user.Image
+import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
+import com.example.realworldkotlinspringbootjdbc.domain.user.Username
+import com.example.realworldkotlinspringbootjdbc.service.UserService
+import com.example.realworldkotlinspringbootjdbc.util.MyError
+import org.springframework.stereotype.Repository
+
+interface UserRepository {
+    fun register(user: UnregisteredUser): Either<UserService.RegisterError, RegisteredUser>
+
+    sealed interface UserRepositoryError : MyError {
+        sealed interface TransactionError : UserRepositoryError {
+            data class DbError(override val cause: Throwable, val unregisteredUser: UnregisteredUser) : UserRepositoryError, MyError.MyErrorWithThrowable
+            data class UnexpectedError(override val cause: Throwable, val unregisteredUser: UnregisteredUser) : UserRepositoryError, MyError.MyErrorWithThrowable
+        }
+        object NotImplemented : UserRepositoryError
+    }
+}
+
+@Repository
+class UserRepositoryImpl : UserRepository {
+    override fun register(user: UnregisteredUser): Either<UserService.RegisterError, RegisteredUser> {
+        val userId = try {
+            registerTransactionApply(user)
+        } catch (e: Throwable) {
+            val error = UserRepository.UserRepositoryError.TransactionError.DbError(e, user)
+            return Either.Left(UserService.RegisterError.FailedRegister(error))
+        }
+        val registeredUser = object : RegisteredUser {
+            override val id: UserId get() = userId
+            override val email: Email get() = user.email
+            override val username: Username get() = user.username
+            override val bio: Bio get() = object : Bio { override val value: String get() = "" }
+            override val image: Image get() = object : Image { override val value: String get() = "" }
+        }
+        return Either.Right(registeredUser)
+    }
+
+    //
+    // @Transaction
+    fun registerTransactionApply(user: UnregisteredUser): UserId {
+        // val sql0 = "SELECT count(email) FROM users WHERE users.email = ?"
+        // val sql1 = "INSERT INTO users(email, username, password, created_at, updated_at) VALUES (:email, :username, :password, :created_at, :updated_at) RETURNING id;"
+        // val sql2 = "INSERT INTO profiles(user_id, bio, image, created_at, updated_at) VALUES (:user_id, :bio, :image, :created_at, :updated_at);"
+        return UserId(999)
+    }
+}
--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/service/UserService.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/service/UserService.kt
@@ -1,7 +1,11 @@
 package com.example.realworldkotlinspringbootjdbc.service
 
 import arrow.core.Either
+import arrow.core.Invalid
+import arrow.core.Valid
 import com.example.realworldkotlinspringbootjdbc.domain.RegisteredUser
+import com.example.realworldkotlinspringbootjdbc.domain.UnregisteredUser
+import com.example.realworldkotlinspringbootjdbc.repository.UserRepository
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 import org.springframework.stereotype.Service
 
@@ -15,8 +19,13 @@ interface UserService {
 }
 
 @Service
-class UserServiceImpl : UserService {
+class UserServiceImpl(
+    val userRepository: UserRepository
+) : UserService {
     override fun register(email: String?, password: String?, username: String?): Either<UserService.RegisterError, RegisteredUser> {
-        return Either.Left(UserService.RegisterError.NotImplemented)
+        return when (val it = UnregisteredUser.new(email, password, username)) {
+            is Invalid -> Either.Left(UserService.RegisterError.ValidationErrors(it.value.errors))
+            is Valid -> userRepository.register(it.value)
+        }
     }
 }
