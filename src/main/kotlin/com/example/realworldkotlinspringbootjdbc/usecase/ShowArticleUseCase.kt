66280b147cfdaa7958b703827ff10e797e560787
Msksgm <msksgm3124@gmail.com>
2022-06-26 07:51:31 +0900

    fix: 引数をプリミティブ型からオブジェクトに変更

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/ShowArticleUseCase.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/ShowArticleUseCase.kt
@@ -6,7 +6,13 @@ import arrow.core.Validated.Valid
 import arrow.core.left
 import arrow.core.right
 import com.example.realworldkotlinspringbootjdbc.domain.Article
+import com.example.realworldkotlinspringbootjdbc.domain.Profile
 import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
+import com.example.realworldkotlinspringbootjdbc.domain.article.Tag
+import com.example.realworldkotlinspringbootjdbc.domain.article.Title
+import com.example.realworldkotlinspringbootjdbc.domain.user.Bio
+import com.example.realworldkotlinspringbootjdbc.domain.user.Image
+import com.example.realworldkotlinspringbootjdbc.domain.user.Username
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 import org.springframework.stereotype.Service
 import java.text.SimpleDateFormat
@@ -15,7 +21,9 @@ import java.util.Date
 interface ShowArticleUseCase {
     fun execute(slug: String?): Either<Error, Article> = Error.NotImplemented.left()
     sealed interface Error : MyError {
-        data class ValidationErrors(override val errors: List<MyError.ValidationError>) : Error, MyError.ValidationErrors
+        data class ValidationErrors(override val errors: List<MyError.ValidationError>) : Error,
+            MyError.ValidationErrors
+
         data class FailedShow(override val cause: MyError) : Error, MyError.MyErrorWithMyError
         data class NotFound(override val cause: MyError) : Error, MyError.MyErrorWithMyError
         object NotImplemented : Error
@@ -29,8 +37,8 @@ class ShowArticleUseCaseImpl() : ShowArticleUseCase {
             is Invalid -> ShowArticleUseCase.Error.ValidationErrors(it.value).left()
             is Valid -> {
                 val a = object : Article {
-                    override val title: String
-                        get() = "hoge-title"
+                    override val title: Title
+                        get() = Title.newWithoutValidation("hoge-title")
                     override val slug: Slug
                         get() = it.value
                     override val body: String
@@ -41,10 +49,15 @@ class ShowArticleUseCaseImpl() : ShowArticleUseCase {
                         get() = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
                     override val description: String
                         get() = "hoge-description"
-                    override val tagList: List<String>
-                        get() = listOf("dragons", "training")
-                    override val author: String
-                        get() = "hoge-author"
+                    override val tagList: List<Tag>
+                        get() = listOf(Tag.newWithoutValidation("dragons"), Tag.newWithoutValidation("training"))
+                    override val author: Profile
+                        get() = Profile.newWithoutValidation(
+                            Username.newWithoutValidation("hoge-username"),
+                            Bio.newWithoutValidation("hoge-bio"),
+                            Image.newWithoutValidation("hoge-image"),
+                            true
+                        )
                     override val favorited: Boolean
                         get() = true
                     override val favoritesCount: Int
