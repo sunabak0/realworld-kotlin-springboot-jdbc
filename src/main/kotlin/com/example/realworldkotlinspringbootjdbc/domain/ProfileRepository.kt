304db198abee546bfb452959e4cbd159dffb4919
Msksgm <msksgm3124@gmail.com>
2022-06-26 15:37:41 +0900

    add: ProfileRepository を追加

---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/ProfileRepository.kt
@@ -0,0 +1,13 @@
+package com.example.realworldkotlinspringbootjdbc.domain
+
+import arrow.core.Either
+import com.example.realworldkotlinspringbootjdbc.domain.user.Username
+import com.example.realworldkotlinspringbootjdbc.util.MyError
+
+class ProfileRepository {
+    fun show(username: Username): Either<ShowError, Profile> = TODO()
+    sealed interface ShowError : MyError {
+        data class NotFoundProfileByUsername(val username: Username) : ShowError, MyError.Basic
+        data class Unexpected(override val cause: Throwable) : ShowError, MyError.MyErrorWithThrowable
+    }
+}
--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/ShowProfileUseCase.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/ShowProfileUseCase.kt
@@ -1,13 +1,13 @@
 package com.example.realworldkotlinspringbootjdbc.usecase
 
 import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 import arrow.core.Validated.Invalid
 import arrow.core.Validated.Valid
 import arrow.core.left
-import arrow.core.right
 import com.example.realworldkotlinspringbootjdbc.domain.Profile
-import com.example.realworldkotlinspringbootjdbc.domain.user.Bio
-import com.example.realworldkotlinspringbootjdbc.domain.user.Image
+import com.example.realworldkotlinspringbootjdbc.domain.ProfileRepository
 import com.example.realworldkotlinspringbootjdbc.domain.user.Username
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 import org.springframework.stereotype.Service
@@ -25,26 +25,27 @@ interface ShowProfileUseCase {
 }
 
 @Service
-class ShowProfileUseCaseImpl() : ShowProfileUseCase {
+class ShowProfileUseCaseImpl(
+    val profileRepository: ProfileRepository
+) : ShowProfileUseCase {
     override fun execute(username: String?): Either<ShowProfileUseCase.Error, Profile> {
         return when (val it = Username.new(username)) {
+            /**
+             * Username が不正
+             */
             is Invalid -> ShowProfileUseCase.Error.InvalidUserName(it.value).left()
-            is Valid -> {
-                val a = object : Profile {
-                    override val username: Username
-                        get() = it.value
-                    override val bio: Bio
-                        get() = object : Bio {
-                            override val value: String get() = "hoge-bio"
-                        }
-                    override val image: Image
-                        get() = object : Image {
-                            override val value: String get() = "hoge-image"
-                        }
-                    override val following: Boolean
-                        get() = true
-                }
-                a.right()
+            /**
+             * Username が適切
+             */
+            is Valid -> when (val showProfileResult = profileRepository.show(it.value)) {
+                /**
+                 * プロフィール取得成功
+                 */
+                is Left -> TODO()
+                /**
+                 * プロフィール取得失敗
+                 */
+                is Right -> TODO()
             }
         }
     }
