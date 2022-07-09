045cd032f11c77b490edbb77a1817276f5017e0e
Msksgm <msksgm3124@gmail.com>
2022-07-09 10:52:36 +0900

    feat: 未ログイン時の list メソッドが必要になっため、オーバーロードの list メソッドを追加

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentRepository.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentRepository.kt
@@ -7,11 +7,20 @@ import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 
 interface CommentRepository {
+    fun list(slug: Slug): Either<ListUnauthorizedError, List<Comment>> = TODO()
+    sealed interface ListUnauthorizedError : MyError {
+        data class NotFoundArticleBySlug(val slug: Slug) : ListUnauthorizedError, MyError.Basic
+        data class Unexpected(override val cause: Throwable, val slug: Slug) :
+            ListUnauthorizedError,
+            MyError.MyErrorWithThrowable
+    }
+
     fun list(slug: Slug, currentUserId: UserId): Either<ListError, List<Comment>> = TODO()
     sealed interface ListError : MyError {
         data class NotFoundArticleBySlug(val slug: Slug) : ListError, MyError.Basic
         data class Unexpected(override val cause: Throwable, val slug: Slug) : ListError, MyError.MyErrorWithThrowable
     }
+
     fun create(body: Body): Either<CreateError, Comment> = TODO()
     sealed interface CreateError : MyError {
         data class Unexpected(override val cause: Throwable, val body: Body) : CreateError, MyError.MyErrorWithThrowable
