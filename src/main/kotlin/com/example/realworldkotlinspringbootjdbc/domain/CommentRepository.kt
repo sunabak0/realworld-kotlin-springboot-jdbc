7c431756e3312beb534dca286e15af9e0f242b49
Msksgm <49427230+Msksgm@users.noreply.github.com>
2022-07-10 07:33:42 +0900

    Revert "Revert "get /articles/{slug}/comments に JWT 認証の分岐を追加""

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentRepository.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentRepository.kt
@@ -3,14 +3,24 @@ package com.example.realworldkotlinspringbootjdbc.domain
 import arrow.core.Either
 import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
 import com.example.realworldkotlinspringbootjdbc.domain.comment.Body
+import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 
 interface CommentRepository {
-    fun list(slug: Slug): Either<ListError, List<Comment>> = TODO()
+    fun list(slug: Slug): Either<ListUnauthorizedError, List<Comment>> = TODO()
+    sealed interface ListUnauthorizedError : MyError {
+        data class NotFoundArticleBySlug(val slug: Slug) : ListUnauthorizedError, MyError.Basic
+        data class Unexpected(override val cause: Throwable, val slug: Slug) :
+            ListUnauthorizedError,
+            MyError.MyErrorWithThrowable
+    }
+
+    fun list(slug: Slug, currentUserId: UserId): Either<ListError, List<Comment>> = TODO()
     sealed interface ListError : MyError {
         data class NotFoundArticleBySlug(val slug: Slug) : ListError, MyError.Basic
         data class Unexpected(override val cause: Throwable, val slug: Slug) : ListError, MyError.MyErrorWithThrowable
     }
+
     fun create(body: Body): Either<CreateError, Comment> = TODO()
     sealed interface CreateError : MyError {
         data class Unexpected(override val cause: Throwable, val body: Body) : CreateError, MyError.MyErrorWithThrowable
