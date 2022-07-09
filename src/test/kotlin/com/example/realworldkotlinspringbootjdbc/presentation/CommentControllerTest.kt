7b2de63f33e9bc6537ab08a6c36bbec9a6f68958
Msksgm <msksgm3124@gmail.com>
2022-07-09 15:33:10 +0900

    fix: 変数名を変更

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/CommentControllerTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/CommentControllerTest.kt
@@ -233,7 +233,13 @@ class CommentControllerTest {
                 override val message: String get() = "DummyValidationError"
                 override val key: String get() = "DummyKey"
             }
-            val listReturnValidationError = object : ListCommentUseCase {
+
+            /**
+             * FIXME
+             * ローカルでは動作するが、Github Actions で動作しない変数名を一時的に mock に修正
+             * 命名規則の方針が決まり次第修正
+             */
+            val mock = object : ListCommentUseCase {
                 override fun execute(
                     slug: String?,
                     currentUser: Option<RegisteredUser>
@@ -243,7 +249,7 @@ class CommentControllerTest {
             }
             val actual = commentController(
                 unauthorizedMyAuth,
-                listReturnValidationError,
+                mock,
                 notImplementedCreateCommentUseCase,
                 notImplementedDeleteCommentUseCase
             ).list(
