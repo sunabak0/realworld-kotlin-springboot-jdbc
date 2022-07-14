2401329af457b8fcb4f7f7e5353bcdb051765540
Msksgm <msksgm3124@gmail.com>
2022-07-15 08:35:17 +0900

    refactory: Table Driven Test に統一するため、「Comment は識別子 CommentId が一致していたら、同値であることを期待する」にを Table Driven Test に移行

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentTest.kt
@@ -3,30 +3,49 @@ package com.example.realworldkotlinspringbootjdbc.domain
 import com.example.realworldkotlinspringbootjdbc.domain.comment.CommentId
 import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 import java.text.SimpleDateFormat
+import java.util.stream.Stream
 import com.example.realworldkotlinspringbootjdbc.domain.comment.Body as CommentBody
 
 class CommentTest {
-    @Test
-    fun `Comment は識別子 CommentId が一致していたら、同値であることを期待する`() {
+    data class TestCase(
+        val title: String,
+        val comment: Comment,
+        val otherComment: Comment,
+        val expect: Boolean
+    )
+
+    @TestFactory
+    fun commentEqualsTest(): Stream<DynamicNode> {
         val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
-        val comment1 = Comment.newWithoutValidation(
-            CommentId.newWithoutValidation(1),
-            CommentBody.newWithoutValidation("dummy-body-1"),
-            createdAt = date,
-            updatedAt = date,
-            UserId(1),
-        )
-        val comment2 = Comment.newWithoutValidation(
-            CommentId.newWithoutValidation(1),
-            CommentBody.newWithoutValidation("dummy-body-2"),
-            createdAt = date,
-            updatedAt = date,
-            UserId(1)
-        )
-        val actual = comment1 == comment2
-        assertThat(actual).isTrue
+        return Stream.of(
+            TestCase(
+                "CommentId が一致してい場合、同値である",
+                Comment.newWithoutValidation(
+                    CommentId.newWithoutValidation(1),
+                    CommentBody.newWithoutValidation("dummy-body-1"),
+                    createdAt = date,
+                    updatedAt = date,
+                    UserId(1),
+                ),
+                Comment.newWithoutValidation(
+                    CommentId.newWithoutValidation(1),
+                    CommentBody.newWithoutValidation("dummy-body-2"),
+                    createdAt = date,
+                    updatedAt = date,
+                    UserId(1)
+                ),
+                true
+            )
+        ).map { testCase ->
+            dynamicTest(testCase.title) {
+                assertThat(testCase.comment == testCase.otherComment).isEqualTo(testCase.expect)
+            }
+        }
     }
 
     @Test
