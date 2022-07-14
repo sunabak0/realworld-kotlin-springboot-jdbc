957a1a698a23f39a931ac4687e837ceebf1423de
Msksgm <msksgm3124@gmail.com>
2022-07-15 08:42:37 +0900

    refactor: Table Driven Test に統一するため、「Comment は識別子 CommentId が一致していない場合、他が同じでも異なる値であることを期待する」にを Table Driven Test に移行

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentTest.kt
@@ -5,7 +5,6 @@ import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest.dynamicTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 import java.text.SimpleDateFormat
 import java.util.stream.Stream
@@ -21,51 +20,49 @@ class CommentTest {
 
     @TestFactory
     fun commentEqualsTest(): Stream<DynamicNode> {
-        val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
+        val date1 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
+        val date2 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-02T00:00:00+09:00")
         return Stream.of(
             TestCase(
-                "CommentId が一致してい場合、同値である",
+                "CommentId が一致する場合、他のプロパティが異なっていても、true を返す（コメントは更新されても、Idが同じなら同一のコメントとして判断される）",
                 Comment.newWithoutValidation(
                     CommentId.newWithoutValidation(1),
                     CommentBody.newWithoutValidation("dummy-body-1"),
-                    createdAt = date,
-                    updatedAt = date,
+                    createdAt = date1,
+                    updatedAt = date1,
                     UserId(1),
                 ),
                 Comment.newWithoutValidation(
                     CommentId.newWithoutValidation(1),
                     CommentBody.newWithoutValidation("dummy-body-2"),
-                    createdAt = date,
-                    updatedAt = date,
+                    createdAt = date1,
+                    updatedAt = date2,
                     UserId(1)
                 ),
                 true
+            ),
+            TestCase(
+                "CommentId が一致しない場合、他のプロパティが全て同じでも、true を返す（コメントが同じタイミングと内容で作成されても、Id が異なれば違うコメントとして判断される）",
+                Comment.newWithoutValidation(
+                    CommentId.newWithoutValidation(1),
+                    CommentBody.newWithoutValidation("dummy-body-1"),
+                    createdAt = date1,
+                    updatedAt = date1,
+                    UserId(1)
+                ),
+                Comment.newWithoutValidation(
+                    CommentId.newWithoutValidation(2),
+                    CommentBody.newWithoutValidation("dummy-body-1"),
+                    createdAt = date1,
+                    updatedAt = date1,
+                    UserId(1)
+                ),
+                false
             )
         ).map { testCase ->
             dynamicTest(testCase.title) {
                 assertThat(testCase.comment == testCase.otherComment).isEqualTo(testCase.expect)
             }
         }
-    }
-
-    @Test
-    fun `Comment は識別子 CommentId が一致していない場合、他が同じでも異なる値であることを期待する`() {
-        val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
-        val comment1 = Comment.newWithoutValidation(
-            CommentId.newWithoutValidation(1),
-            CommentBody.newWithoutValidation("dummy-body-1"),
-            createdAt = date,
-            updatedAt = date,
-            UserId(1)
-        )
-        val comment2 = Comment.newWithoutValidation(
-            CommentId.newWithoutValidation(2),
-            CommentBody.newWithoutValidation("dummy-body-1"),
-            createdAt = date,
-            updatedAt = date,
-            UserId(1)
-        )
-        val actual = comment1 != comment2
-        assertThat(actual).isTrue
     }
 }
