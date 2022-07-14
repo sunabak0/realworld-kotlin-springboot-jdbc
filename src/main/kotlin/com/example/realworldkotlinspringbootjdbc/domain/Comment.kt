ee33c9587f3eb7bd57c11efab74d2648eea7c003
Msksgm <msksgm3124@gmail.com>
2022-07-15 06:37:00 +0900

    refactor: Comment は値オブジェクトではなくエンティティのため、interface から class に変更

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/Comment.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/Comment.kt
@@ -4,32 +4,32 @@ import com.example.realworldkotlinspringbootjdbc.domain.comment.CommentId
 import java.util.Date
 import com.example.realworldkotlinspringbootjdbc.domain.comment.Body as CommentBody
 
-interface Comment {
-    val id: CommentId
-    val body: CommentBody
-    val createdAt: Date
-    val updatedAt: Date
+class Comment private constructor(
+    val id: CommentId,
+    val body: CommentBody,
+    val createdAt: Date,
+    val updatedAt: Date,
     val author: OtherUser
-
-    /**
-     * 実装
-     */
-    private data class CommentWithoutValidation(
-        override val id: CommentId,
-        override val body: CommentBody,
-        override val createdAt: Date,
-        override val updatedAt: Date,
-        override val author: OtherUser
-    ) : Comment
-
-    /**
-     * Factory メソッド
-     */
+) {
     companion object {
-        /**
-         * Validation 無し
-         */
-        fun newWithoutValidation(id: CommentId, body: CommentBody, createdAt: Date, updatedAt: Date, author: OtherUser): Comment =
-            CommentWithoutValidation(id, body, createdAt, updatedAt, author)
+        fun newWithoutValidation(
+            id: CommentId,
+            body: CommentBody,
+            createdAt: Date,
+            updatedAt: Date,
+            author: OtherUser
+        ): Comment =
+            Comment(id, body, createdAt, updatedAt, author)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Comment
+        return this.id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.value * 31
     }
 }
