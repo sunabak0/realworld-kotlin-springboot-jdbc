b5cb160565803f5dfe7dacd50fef6493924adb70
Msksgm <msksgm3124@gmail.com>
2022-07-16 15:14:02 +0900

    feat: Article をエンティティ（class）にした変更に伴い作成したプロパティのファクトリメソッドを追加

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/Body.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/Body.kt
@@ -1,4 +1,20 @@
 package com.example.realworldkotlinspringbootjdbc.domain.article
 
 interface Body {
+    val value: String
+
+    /**
+     * 実装
+     */
+    private data class BodyWithoutValidation(override val value: String) : Body
+
+    /**
+     * Factory メソッド
+     */
+    companion object {
+        /**
+         * Validation 無し
+         */
+        fun newWithoutValidation(body: String): Body = BodyWithoutValidation(body)
+    }
 }
--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/Description.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/Description.kt
@@ -1,4 +1,20 @@
 package com.example.realworldkotlinspringbootjdbc.domain.article
 
 interface Description {
+    val value: String
+
+    /**
+     * 実装
+     */
+    private data class DescriptionWithoutValidation(override val value: String) : Description
+
+    /**
+     * Factory メソッド
+     */
+    companion object {
+        /**
+         * Validation 無し
+         */
+        fun newWithoutValidation(description: String): Description = DescriptionWithoutValidation(description)
+    }
 }
