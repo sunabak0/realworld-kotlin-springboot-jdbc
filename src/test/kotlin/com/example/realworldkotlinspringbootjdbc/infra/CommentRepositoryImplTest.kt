92e970ce2edf4d094435e2d241433573e3b5bb97
Msksgm <msksgm3124@gmail.com>
2022-07-16 11:11:09 +0900

    test: 将来的に実装するため、「異常系-原因不明のため UnexpectedError が戻り値」のテストを追加

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/infra/CommentRepositoryImplTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/infra/CommentRepositoryImplTest.kt
@@ -212,7 +212,7 @@ class CommentRepositoryImplTest {
 
         @Test
         @Disabled
-        fun `異常系-UnexpectedError が戻り値`() {
+        fun `異常系-原因不明のため UnexpectedError が戻り値`() {
             TODO()
         }
     }
@@ -292,7 +292,7 @@ class CommentRepositoryImplTest {
         }
 
         @Test
-        fun `異常系-articles テーブルに slug に該当する記事が存在しない場合、戻り値が NotFoundError`(){
+        fun `異常系-articles テーブルに slug に該当する記事が存在しない場合、戻り値が NotFoundError`() {
             /**
              * 実行前に挿入されていないことを確認
              */
@@ -308,6 +308,12 @@ class CommentRepositoryImplTest {
                 is Left -> assertThat(actual.value).isEqualTo(expected)
                 is Right -> assert(false)
             }
+        }
+
+        @Test
+        @Disabled
+        fun `異常系-原因不明のため UnexpectedError が戻り値`() {
+            TODO()
         }
     }
 }
