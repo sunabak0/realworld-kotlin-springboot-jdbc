371b5cc2e7a15e102ebcfa1789fb9a144e1a141d
Msksgm <msksgm3124@gmail.com>
2022-07-16 15:15:57 +0900

    fix: Article をエンティティの変更にしたため、呼び出し側の生成方法を変更

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/ShowArticleUseCase.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/ShowArticleUseCase.kt
@@ -5,19 +5,17 @@ import arrow.core.Validated.Invalid
 import arrow.core.Validated.Valid
 import arrow.core.left
 import arrow.core.right
+import com.example.realworldkotlinspringbootjdbc.domain.ArticleId
 import com.example.realworldkotlinspringbootjdbc.domain.CreatedArticle
-import com.example.realworldkotlinspringbootjdbc.domain.OtherUser
+import com.example.realworldkotlinspringbootjdbc.domain.article.Description
+import com.example.realworldkotlinspringbootjdbc.domain.article.Body as ArticleBody
 import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
 import com.example.realworldkotlinspringbootjdbc.domain.article.Tag
 import com.example.realworldkotlinspringbootjdbc.domain.article.Title
-import com.example.realworldkotlinspringbootjdbc.domain.user.Bio
-import com.example.realworldkotlinspringbootjdbc.domain.user.Image
 import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
-import com.example.realworldkotlinspringbootjdbc.domain.user.Username
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 import org.springframework.stereotype.Service
 import java.text.SimpleDateFormat
-import java.util.Date
 
 interface ShowArticleUseCase {
     fun execute(slug: String?): Either<Error, CreatedArticle> = Error.NotImplemented.left()
@@ -38,34 +36,19 @@ class ShowArticleUseCaseImpl() : ShowArticleUseCase {
         return when (val it = Slug.new(slug)) {
             is Invalid -> ShowArticleUseCase.Error.ValidationErrors(it.value).left()
             is Valid -> {
-                val a = object : CreatedArticle {
-                    override val title: Title
-                        get() = Title.newWithoutValidation("hoge-title")
-                    override val slug: Slug
-                        get() = it.value
-                    override val body: String
-                        get() = "hoge-body"
-                    override val createdAt: Date
-                        get() = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
-                    override val updatedAt: Date
-                        get() = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
-                    override val description: String
-                        get() = "hoge-description"
-                    override val tagList: List<Tag>
-                        get() = listOf(Tag.newWithoutValidation("dragons"), Tag.newWithoutValidation("training"))
-                    override val author: OtherUser
-                        get() = OtherUser.newWithoutValidation(
-                            UserId(1),
-                            Username.newWithoutValidation("hoge-username"),
-                            Bio.newWithoutValidation("hoge-bio"),
-                            Image.newWithoutValidation("hoge-image"),
-                            true,
-                        )
-                    override val favorited: Boolean
-                        get() = true
-                    override val favoritesCount: Int
-                        get() = 1
-                }
+                val a = CreatedArticle.newWithoutValidation(
+                    ArticleId(1),
+                    Title.newWithoutValidation("hoge-title"),
+                    it.value,
+                    ArticleBody.newWithoutValidation("hoge-body"),
+                    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00"),
+                    SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00"),
+                    Description.newWithoutValidation("hoge-dscription"),
+                    listOf(Tag.newWithoutValidation("dragons"), Tag.newWithoutValidation("training")),
+                    UserId(1),
+                    true,
+                    1
+                )
                 a.right()
             }
         }
