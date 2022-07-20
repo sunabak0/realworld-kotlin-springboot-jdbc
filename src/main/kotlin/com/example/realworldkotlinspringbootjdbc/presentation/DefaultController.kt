2ccc7f726fe4351596d784acf16faa6ca61443d0
sunakan <sunakan03@gmail.com>
2022-07-20 13:29:00 +0900

    タグ一覧 プレゼンテーション層の実装とテスト記述

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/DefaultController.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/DefaultController.kt
@@ -1,6 +1,10 @@
 package com.example.realworldkotlinspringbootjdbc.presentation
 
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 import com.example.realworldkotlinspringbootjdbc.presentation.response.Tags
+import com.example.realworldkotlinspringbootjdbc.presentation.response.serializeUnexpectedErrorForResponseBody
+import com.example.realworldkotlinspringbootjdbc.usecase.ListTagUseCase
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
@@ -10,15 +14,38 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Tag(name = "default")
-class DefaultController {
+class DefaultController(
+    val listTagUseCase: ListTagUseCase
+) {
+    /**
+     * タグ一覧
+     *
+     * ```
+     * curl -X GET --header 'Content-Type: application/json' 'http://localhost:8080/api/tags' | jq '.'
+     * ```
+     */
     @GetMapping("/tags")
     fun list(): ResponseEntity<String> {
-        val tags = Tags(
-            listOf("dragons", "training"),
-        )
-        return ResponseEntity(
-            ObjectMapper().writeValueAsString(tags),
-            HttpStatus.valueOf(200),
-        )
+        return when (val result = listTagUseCase.execute()) {
+            /**
+             * 失敗
+             */
+            is Left -> when (result.value) {
+                /**
+                 * 原因: 不明
+                 */
+                is ListTagUseCase.Error.Unexpected -> ResponseEntity(
+                    serializeUnexpectedErrorForResponseBody("原因不明のエラーが発生しました"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
+                    HttpStatus.valueOf(500)
+                )
+            }
+            /**
+             * 成功
+             */
+            is Right -> ResponseEntity(
+                ObjectMapper().writeValueAsString(Tags(result.value.map { it.value })),
+                HttpStatus.valueOf(200),
+            )
+        }
     }
 }
