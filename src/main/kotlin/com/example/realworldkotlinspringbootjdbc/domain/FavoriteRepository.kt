faa55f34d4dfa6ca824b18b081e331e9f3f2a253
Msksgm <msksgm3124@gmail.com>
2022-07-31 06:24:55 +0900

    feat: FavoriteRepository.favorite を追加し、FavoriteUseCase の分岐を記述

---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/FavoriteRepository.kt
@@ -0,0 +1,16 @@
+package com.example.realworldkotlinspringbootjdbc.domain
+
+import arrow.core.Either
+import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
+import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
+import com.example.realworldkotlinspringbootjdbc.util.MyError
+
+interface FavoriteRepository {
+    fun favorite(slug: Slug, currentUserId: UserId): Either<FavoriteError, CreatedArticle> = TODO()
+    sealed interface FavoriteError : MyError {
+        data class ArticleNotFoundBySlug(val slug: Slug) : FavoriteError, MyError.Basic
+        data class Unexpected(override val cause: Throwable, val slug: Slug) :
+            FavoriteError,
+            MyError.MyErrorWithThrowable
+    }
+}
