5957fd5a503500dbeecbe9f57ea1216907a25c52
Msksgm <49427230+Msksgm@users.noreply.github.com>
2022-08-03 08:26:02 +0900

    post /articles/{slug}/favorite の実装/テストを作成 (#110)
    * feat: 「記事のお気に入り」機能を追加するため、FavoriteController.favorite と FavoriteUseCase.execute を実装
    
    - FavoriteController.favorite 「記事のお気に入り」までのハッピーパスを追加
    - FavoriteUseCase.execute ユースケース「記事のお気に入りの分岐」を追加
    
    * feat: FavoriteUseCase に 「Slug に該当する記事が見つからなかった場合」のエラーを追加
    
    * feat: FavoriteController に slug が不正だったときの分岐を追加
    
    * feat: FavoriteController に slug に該当する記事が見つからなかったときの分岐を追加
    
    * feat: FavoriteController に原因不明のエラーが発生したときの分岐を追加
    
    * feat: FavoriteRepository.favorite を追加し、FavoriteUseCase の分岐を記述
    
    * fix: Unexpected の引数に、val currentUserId: UserId を追加
    
    * refactor: エラーの戻り値を「XXXNot」ではなく「NotXXX」に統一
    
    * refactor: FavoriteRepository の責務は、ArticleRepository だったので favorite メソッドを移動
    
    * refactor: エラーはNotXXX に統一するため、NotFounArticleBySlug に修正

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/FavoriteController.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/FavoriteController.kt
@@ -1,37 +1,96 @@
 package com.example.realworldkotlinspringbootjdbc.presentation
 
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 import com.example.realworldkotlinspringbootjdbc.presentation.response.Article
+import com.example.realworldkotlinspringbootjdbc.presentation.response.serializeUnexpectedErrorForResponseBody
+import com.example.realworldkotlinspringbootjdbc.presentation.shared.AuthorizationError
+import com.example.realworldkotlinspringbootjdbc.usecase.favorite.FavoriteUseCase
+import com.example.realworldkotlinspringbootjdbc.util.MyAuth
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import java.text.SimpleDateFormat
 
 @RestController
 @Tag(name = "Favorites")
-class FavoriteController {
+class FavoriteController(
+    val myAuth: MyAuth,
+    val favoriteUseCase: FavoriteUseCase,
+) {
     @PostMapping("/articles/{slug}/favorite")
-    fun favorite(): ResponseEntity<String> {
-        val article = Article(
-            "hoge-title",
-            "hoge-slug",
-            "hoge-body",
-            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00"),
-            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00"),
-            "hoge-description",
-            listOf("dragons", "training"),
-            "hoge-author",
-            true,
-            1,
-        )
-        return ResponseEntity(
-            ObjectMapper().enable(SerializationFeature.WRAP_ROOT_VALUE).writeValueAsString(article),
-            HttpStatus.valueOf(200),
-        )
+    fun favorite(
+        @RequestHeader("Authorization") rawAuthorizationHeader: String?,
+        @PathVariable("slug") slug: String?
+    ): ResponseEntity<String> {
+        return when (val authorizeResult = myAuth.authorize(rawAuthorizationHeader)) {
+            /**
+             * JWT 認証 失敗
+             */
+            is Left -> AuthorizationError.handle(authorizeResult.value)
+            /**
+             * JWT 認証 成功
+             */
+            is Right -> {
+                when (val favoritedArticle = favoriteUseCase.execute(slug, authorizeResult.value)) {
+                    /**
+                     * お気に入り登録 失敗
+                     */
+                    is Left -> when (favoritedArticle.value) {
+                        /**
+                         * 原因: Slug がバリデーションエラー
+                         */
+                        is FavoriteUseCase.Error.InvalidSlug -> ResponseEntity(
+                            serializeUnexpectedErrorForResponseBody("記事が見つかりませんでした"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
+                            HttpStatus.valueOf(404)
+                        )
+                        /**
+                         * 原因: 記事が見つからなかった
+                         */
+                        is FavoriteUseCase.Error.NotFoundArticleBySlug -> ResponseEntity(
+                            serializeUnexpectedErrorForResponseBody("記事が見つかりませんでした"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
+                            HttpStatus.valueOf(404)
+                        )
+                        /**
+                         * 原因: 不明
+                         */
+                        is FavoriteUseCase.Error.Unexpected -> ResponseEntity(
+                            serializeUnexpectedErrorForResponseBody("原因不明のエラーが発生しました"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
+                            HttpStatus.valueOf(500)
+                        )
+                    }
+                    /**
+                     * お気に入り登録 成功
+                     */
+                    is Right -> {
+                        val article = Article(
+                            favoritedArticle.value.title.value,
+                            favoritedArticle.value.slug.value,
+                            favoritedArticle.value.body.value,
+                            favoritedArticle.value.createdAt,
+                            favoritedArticle.value.updatedAt,
+                            favoritedArticle.value.description.value,
+                            favoritedArticle.value.tagList.map { tag -> tag.value },
+                            // TODO: authorId を author に変更
+                            favoritedArticle.value.authorId.value.toString(),
+                            favoritedArticle.value.favorited,
+                            favoritedArticle.value.favoritesCount
+                        )
+                        return ResponseEntity(
+                            ObjectMapper().enable(SerializationFeature.WRAP_ROOT_VALUE).writeValueAsString(article),
+                            HttpStatus.valueOf(200),
+                        )
+                    }
+                }
+            }
+        }
     }
 
     @DeleteMapping("/articles/{slug}/favorite")
---
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/favorite/FavoriteUseCase.kt
@@ -0,0 +1,62 @@
+package com.example.realworldkotlinspringbootjdbc.usecase.favorite
+
+import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import arrow.core.Invalid
+import arrow.core.Valid
+import arrow.core.left
+import com.example.realworldkotlinspringbootjdbc.domain.ArticleRepository
+import com.example.realworldkotlinspringbootjdbc.domain.CreatedArticle
+import com.example.realworldkotlinspringbootjdbc.domain.RegisteredUser
+import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
+import com.example.realworldkotlinspringbootjdbc.util.MyError
+import org.springframework.stereotype.Service
+
+interface FavoriteUseCase {
+    fun execute(slug: String?, currentUser: RegisteredUser): Either<Error, CreatedArticle> = TODO()
+    sealed interface Error : MyError {
+        data class InvalidSlug(override val errors: List<MyError.ValidationError>) : Error, MyError.ValidationErrors
+        data class NotFoundArticleBySlug(override val cause: MyError, val slug: Slug) :
+            Error,
+            MyError.MyErrorWithMyError
+
+        data class Unexpected(override val cause: MyError) : Error, MyError.MyErrorWithMyError
+    }
+}
+
+@Service
+class FavoriteUseCaseImpl(
+    val articleRepository: ArticleRepository
+) : FavoriteUseCase {
+    override fun execute(slug: String?, currentUser: RegisteredUser): Either<FavoriteUseCase.Error, CreatedArticle> {
+        return when (val it = Slug.new(slug)) {
+            /**
+             * Slug が不正
+             */
+            is Invalid -> FavoriteUseCase.Error.InvalidSlug(it.value).left()
+            /**
+             * Slug が適切
+             */
+            is Valid -> when (val favoriteResult = articleRepository.favorite(it.value, currentUser.userId)) {
+                /**
+                 * お気に入り追加 失敗
+                 */
+                is Left -> when (favoriteResult.value) {
+                    /**
+                     * 原因: 作成済記事が見つからなかった
+                     */
+                    is ArticleRepository.FavoriteError.NotFoundCreatedArticleBySlug -> TODO()
+                    /**
+                     * 原因: 不明
+                     */
+                    is ArticleRepository.FavoriteError.Unexpected -> TODO()
+                }
+                /**
+                 * お気に入り追加 成功
+                 */
+                is Right -> favoriteResult
+            }
+        }
+    }
+}
