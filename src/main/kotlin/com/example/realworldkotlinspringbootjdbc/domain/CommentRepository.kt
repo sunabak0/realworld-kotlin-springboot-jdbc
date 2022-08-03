5abe4fc0f861f5163d303f459dbacb113dedc6be
Msksgm <49427230+Msksgm@users.noreply.github.com>
2022-08-04 06:37:19 +0900

    CommentRepositoryImpl.list の実装/テスト を追加 (#107)
    * fix: コメントの取得はログインユーザーの情報を必要としないので、list のオーバーロードを削除
    
    * style: Return の数を 4 から 5 に変更
    
    * feat: 「コメントの取得」機能を追加
    
    * test: CommentRepository.list の戻り値が空リストだったときのテストを追加
    
    * test: CommentRepository.list の戻り値が「Slug に該当する作成済記事がなかった」ときのテストを追加
    
    * refactor: 空の comments_articles を用意する必要がなかっため、削除
    
    * style: 列名を明示するように、AS author_id を記述
    
    * refactor: 同じ型は混合しなように、キーワード引数を記述
    
    - created_at と　updated_at を記述
    
    * test: コメントの書き方を // から /* に変更
    
    * test: テスト名のタイポを修正
    
    * test: テスト名を「異常系」から「準正常系」に修正
    
    * test: テストは引数の記述を必須
    
    * refactor: SQL を統一するために、id、セミコロン、表明を明記
    
    * refactor: 戻り値がList 型なのと、後で isEmpty を使うため、名前を articleList に修正
    
    * test: when 式を使わない方法に変更

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentRepository.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/domain/CommentRepository.kt
@@ -3,19 +3,10 @@ package com.example.realworldkotlinspringbootjdbc.domain
 import arrow.core.Either
 import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
 import com.example.realworldkotlinspringbootjdbc.domain.comment.Body
-import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 
 interface CommentRepository {
-    fun list(slug: Slug): Either<ListUnauthorizedError, List<Comment>> = TODO()
-    sealed interface ListUnauthorizedError : MyError {
-        data class NotFoundArticleBySlug(val slug: Slug) : ListUnauthorizedError, MyError.Basic
-        data class Unexpected(override val cause: Throwable, val slug: Slug) :
-            ListUnauthorizedError,
-            MyError.MyErrorWithThrowable
-    }
-
-    fun list(slug: Slug, currentUserId: UserId): Either<ListError, List<Comment>> = TODO()
+    fun list(slug: Slug): Either<ListError, List<Comment>> = TODO()
     sealed interface ListError : MyError {
         data class NotFoundArticleBySlug(val slug: Slug) : ListError, MyError.Basic
         data class Unexpected(override val cause: Throwable, val slug: Slug) : ListError, MyError.MyErrorWithThrowable
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/infra/CommentRepositoryImplTest.kt
@@ -0,0 +1,161 @@
+package com.example.realworldkotlinspringbootjdbc.infra
+
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import arrow.core.left
+import arrow.core.right
+import com.example.realworldkotlinspringbootjdbc.domain.Comment
+import com.example.realworldkotlinspringbootjdbc.domain.CommentRepository
+import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
+import com.example.realworldkotlinspringbootjdbc.domain.comment.Body
+import com.example.realworldkotlinspringbootjdbc.domain.comment.CommentId
+import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
+import com.github.database.rider.core.api.dataset.DataSet
+import com.github.database.rider.junit5.api.DBRider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import java.text.SimpleDateFormat
+
+class CommentRepositoryImplTest {
+    companion object {
+        val namedParameterJdbcTemplate = DbConnection.namedParameterJdbcTemplate
+
+        fun resetSequence() {
+            val sql = """
+                SELECT
+                    setval('articles_id_seq', 10000)
+                    , setval('tags_id_seq', 10000)
+                    , setval('article_comments_id_seq', 10000)
+                ;
+            """.trimIndent()
+            DbConnection.namedParameterJdbcTemplate.queryForRowSet(sql, MapSqlParameterSource())
+        }
+    }
+
+    @Tag("WithLocalDb")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DBRider
+    @DisplayName("List(コメント一覧を表示)")
+    class List {
+        @BeforeAll
+        fun reset() = resetSequence()
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/articles.yml"
+            ],
+        )
+        fun `正常系-articles テーブルに slug に該当する作成済記事（CreatedArticle）が存在した場合、コメント（Comment） の List が戻り値`() {
+            /**
+             * given
+             */
+            val commentRepository = CommentRepositoryImpl(namedParameterJdbcTemplate)
+
+            /**
+             * when
+             */
+            val actual = commentRepository.list(Slug.newWithoutValidation("rust-vs-scala-vs-kotlin"))
+
+            /**
+             * then
+             */
+            val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("2022-01-01T00:00:00+09:00")
+            val expected = listOf(
+                Comment.newWithoutValidation(
+                    id = CommentId.newWithoutValidation(1),
+                    body = Body.newWithoutValidation("dummy-comment-body-01"),
+                    createdAt = date,
+                    updatedAt = date,
+                    authorId = UserId(3),
+                ),
+                Comment.newWithoutValidation(
+                    id = CommentId.newWithoutValidation(3),
+                    body = Body.newWithoutValidation("dummy-comment-body-03"),
+                    createdAt = date,
+                    updatedAt = date,
+                    authorId = UserId(2),
+                ),
+                Comment.newWithoutValidation(
+                    id = CommentId.newWithoutValidation(5),
+                    body = Body.newWithoutValidation("dummy-comment-body-02"),
+                    createdAt = date,
+                    updatedAt = date,
+                    authorId = UserId(3),
+                ),
+            )
+            when (actual) {
+                is Left -> assert(false)
+                is Right -> {
+                    // List<Comment> 1 つめの比較（createdAt, updateAtはメタデータなので比較しない）
+                    assertThat(actual.value[0].id).isEqualTo(expected[0].id)
+                    assertThat(actual.value[0].body).isEqualTo(expected[0].body)
+                    assertThat(actual.value[0].authorId).isEqualTo(expected[0].authorId)
+                    // List<Comment> 2 つめの比較（createdAt, updateAtはメタデータなので比較しない）
+                    assertThat(actual.value[1].id).isEqualTo(expected[1].id)
+                    assertThat(actual.value[1].body).isEqualTo(expected[1].body)
+                    assertThat(actual.value[1].authorId).isEqualTo(expected[1].authorId)
+                    // List<Comment> 3 つめの比較（createdAt, updateAtはメタデータなので比較しない）
+                    assertThat(actual.value[2].id).isEqualTo(expected[2].id)
+                    assertThat(actual.value[2].body).isEqualTo(expected[2].body)
+                    assertThat(actual.value[2].authorId).isEqualTo(expected[2].authorId)
+                }
+            }
+        }
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/articles.yml",
+            ],
+        )
+        fun `正常系-articles テーブルに slug に該当するが Comment 存在しなかった場合、空の Comment の List が戻り値`() {
+            /**
+             * given:
+             */
+            val commentRepository = CommentRepositoryImpl(namedParameterJdbcTemplate)
+
+            /**
+             * when:
+             */
+            val actual = commentRepository.list(Slug.newWithoutValidation("functional-programming-kotlin"))
+
+            /**
+             * then:
+             */
+            val expected = listOf<Comment>().right()
+            assertThat(actual).isEqualTo(expected)
+        }
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/empty-articles.yml",
+            ],
+        )
+        fun `準正常系-articles テーブルに slug に該当する記事がなかった場合、NotFoundError が戻り値`() {
+            /**
+             * given:
+             */
+            val commentRepository = CommentRepositoryImpl(namedParameterJdbcTemplate)
+
+            /**
+             * when:
+             */
+            val actual = commentRepository.list(Slug.newWithoutValidation("not-found-article-slug"))
+
+            /**
+             * then:
+             */
+            val expected =
+                CommentRepository.ListError.NotFoundArticleBySlug(Slug.newWithoutValidation("not-found-article-slug"))
+                    .left()
+            assertThat(actual).isEqualTo(expected)
+        }
+    }
+}
