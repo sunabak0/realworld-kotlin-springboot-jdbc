bb4841980e07f2eb7c2ca17faa49dde2b1032b46
sunakan <sunakan03@gmail.com>
2022-08-16 06:34:41 +0900

    🐘🆙Gradleのplugin.spring と jvmを1.6.21 -> 1.7.10 (#146)
    * build(deps): bump jvm from 1.6.21 to 1.7.10
    
    Bumps [jvm](https://github.com/JetBrains/kotlin) from 1.6.21 to 1.7.10.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/v1.7.10/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v1.6.21...v1.7.10)
    
    ---
    updated-dependencies:
    - dependency-name: jvm
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump plugin.spring from 1.6.21 to 1.7.10
    
    Bumps [plugin.spring](https://github.com/JetBrains/kotlin) from 1.6.21 to 1.7.10.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/v1.7.10/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v1.6.21...v1.7.10)
    
    ---
    updated-dependencies:
    - dependency-name: plugin.spring
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * jvmとplugin.springのバージョンアップに対して注意事項を記述
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: sunakan <>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.2"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    /**
+     * 注意
+     * jvm と plugin.spring のバージョンは合わせること
+     */
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
 
     /**
      * detekt
