2a3a598e431ddfe38ef58770e3e2fbcb8ac655ec
Msksgm <msksgm3124@gmail.com>
2022-08-27 15:18:09 +0900

    feat: 作成済記事（CreatedArticle）のドメインオブジェクト Body を作成

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/BodyTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/BodyTest.kt
@@ -1,10 +1,37 @@
 package com.example.realworldkotlinspringbootjdbc.domain.article
 
+import arrow.core.invalid
+import arrow.core.invalidNel
+import arrow.core.valid
 import net.jqwik.api.Arbitraries
 import net.jqwik.api.Arbitrary
 import net.jqwik.api.ArbitrarySupplier
+import net.jqwik.api.ForAll
+import net.jqwik.api.Property
+import net.jqwik.api.constraints.StringLength
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
 class BodyTest {
+    @Test
+    fun `CreatedArticle の Body は必須項目`() {
+        val invalid = Body.ValidationError.Required.check(null)
+        val valid = Body.ValidationError.Required.check("")
+        assertThat(invalid).isEqualTo(Body.ValidationError.Required.invalid())
+        assertThat(valid).isEqualTo("".valid())
+    }
+
+    @Property(tries = 100)
+    fun `CreatedArticle の Body の長さは 1024 文字以下`(
+        @ForAll @StringLength(min = 1025) invalidBody: String,
+        @ForAll @StringLength(max = 1024) validBody: String,
+    ) {
+        val invalid = Body.ValidationError.TooLong.check(invalidBody)
+        val valid = Body.ValidationError.TooLong.check(validBody)
+        assertThat(invalid).isEqualTo(Body.ValidationError.TooLong(invalidBody).invalidNel())
+        assertThat(valid).isEqualTo(Unit.valid())
+    }
+
     /**
      * Bodyの有効な範囲のStringプロパティ
      */
