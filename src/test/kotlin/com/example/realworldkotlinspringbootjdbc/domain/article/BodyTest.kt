d68197bb395cb55258fa231854dbb2115ee90810
sunakan <>
2022-09-03 13:46:49 +0900

    CreatedArticleのBodyTestの修正
    Bodyの有効な範囲用のクラスを用意

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/BodyTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/BodyTest.kt
@@ -1,35 +1,79 @@
 package com.example.realworldkotlinspringbootjdbc.domain.article
 
-import arrow.core.invalid
+import arrow.core.Invalid
+import arrow.core.Valid
 import arrow.core.invalidNel
-import arrow.core.valid
 import net.jqwik.api.Arbitraries
 import net.jqwik.api.Arbitrary
 import net.jqwik.api.ArbitrarySupplier
 import net.jqwik.api.ForAll
+import net.jqwik.api.From
 import net.jqwik.api.Property
 import net.jqwik.api.constraints.StringLength
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BodyTest {
-    @Test
-    fun `CreatedArticle の Body は必須項目`() {
-        val invalid = Body.ValidationError.Required.check(null)
-        val valid = Body.ValidationError.Required.check("")
-        assertThat(invalid).isEqualTo(Body.ValidationError.Required.invalid())
-        assertThat(valid).isEqualTo("".valid())
+
+    @Property
+    fun `正常系-長さが有効な場合、検証済みの作成済み記事のBodyが戻り値`(
+        @ForAll @From(supplier = BodyValidRange::class) validString: String,
+    ) {
+        /**
+         * given:
+         */
+
+        /**
+         * when:
+         */
+        val actual = Body.new(validString)
+
+        /**
+         * then:
+         */
+        when (actual) {
+            is Invalid -> assert(false) { "原因: ${actual.value}" }
+            is Valid -> assertThat(actual.value.value).isEqualTo(validString)
+        }
     }
 
-    @Property(tries = 100)
-    fun `CreatedArticle の Body の長さは 1024 文字以下`(
-        @ForAll @StringLength(min = 1025) invalidBody: String,
-        @ForAll @StringLength(max = 1024) validBody: String,
+    @Property
+    fun `準正常系-長さが長すぎる場合、バリデーションエラーが戻り値`(
+        @ForAll @StringLength(min = 1025) tooLongString: String
     ) {
-        val invalid = Body.ValidationError.TooLong.check(invalidBody)
-        val valid = Body.ValidationError.TooLong.check(validBody)
-        assertThat(invalid).isEqualTo(Body.ValidationError.TooLong(invalidBody).invalidNel())
-        assertThat(valid).isEqualTo(Unit.valid())
+        /**
+         * given:
+         */
+
+        /**
+         * when:
+         */
+        val actual = Body.new(tooLongString)
+
+        /**
+         * then:
+         */
+        val expected = Body.ValidationError.TooLong(tooLongString).invalidNel()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `準正常系-nullの場合、バリデーションエラーが戻り値`() {
+        /**
+         * given:
+         */
+        val nullString = null
+
+        /**
+         * when:
+         */
+        val actual = Body.new(nullString)
+
+        /**
+         * then:
+         */
+        val expected = Body.ValidationError.Required.invalidNel()
+        assertThat(actual).isEqualTo(expected)
     }
 
     /**
@@ -38,6 +82,8 @@ class BodyTest {
     class BodyValidRange : ArbitrarySupplier<String> {
         override fun get(): Arbitrary<String> =
             Arbitraries.strings()
+                .ofMinLength(0)
+                .ofMaxLength(1024)
                 .filter { !it.startsWith("diff-") }
     }
 }
