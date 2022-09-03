15714355293278fc4fc9f6a2630255fe55ef1ec1
sunakan <>
2022-09-03 14:00:28 +0900

    CreatedArticleのDescriptionTestの修正
    Descriptionの有効な範囲用のクラスを用意

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/DescriptionTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/domain/article/DescriptionTest.kt
@@ -1,35 +1,78 @@
 package com.example.realworldkotlinspringbootjdbc.domain.article
 
-import arrow.core.invalid
+import arrow.core.Invalid
+import arrow.core.Valid
 import arrow.core.invalidNel
-import arrow.core.valid
 import net.jqwik.api.Arbitraries
 import net.jqwik.api.Arbitrary
 import net.jqwik.api.ArbitrarySupplier
 import net.jqwik.api.ForAll
+import net.jqwik.api.From
 import net.jqwik.api.Property
 import net.jqwik.api.constraints.StringLength
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class DescriptionTest {
-    @Test
-    fun `CreatedArticle の Description は必須項目`() {
-        val invalid = Description.ValidationError.Required.check(null)
-        val valid = Description.ValidationError.Required.check("")
-        assertThat(invalid).isEqualTo(Description.ValidationError.Required.invalid())
-        assertThat(valid).isEqualTo("".valid())
+    @Property
+    fun `正常系-長さが有効な場合、検証済みの作成済み記事のDescriptionが戻り値`(
+        @ForAll @From(supplier = DescriptionValidRange::class) validString: String
+    ) {
+        /**
+         * given:
+         */
+
+        /**
+         * when:
+         */
+        val actual = Description.new(validString)
+
+        /**
+         * then:
+         */
+        when (actual) {
+            is Invalid -> assert(false) { "原因: ${actual.value}" }
+            is Valid -> assertThat(actual.value.value).isEqualTo(validString)
+        }
     }
 
     @Property
-    fun `CreatedArticle の Description の長さは 64 文字以下`(
-        @ForAll @StringLength(min = 65) invalidDescription: String,
-        @ForAll @StringLength(max = 64) validDescription: String,
+    fun `準正常系-長さが長すぎる場合、バリデーションエラーが戻り値`(
+        @ForAll @StringLength(min = 65) tooLongString: String
     ) {
-        val invalid = Description.ValidationError.TooLong.check(invalidDescription)
-        val valid = Description.ValidationError.TooLong.check(validDescription)
-        assertThat(invalid).isEqualTo(Description.ValidationError.TooLong(invalidDescription).invalidNel())
-        assertThat(valid).isEqualTo(Unit.valid())
+        /**
+         * given:
+         */
+
+        /**
+         * when:
+         */
+        val actual = Description.new(tooLongString)
+
+        /**
+         * then:
+         */
+        val expected = Description.ValidationError.TooLong(tooLongString).invalidNel()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `準正常系-nullの場合、バリデーションエラーが戻り値`() {
+        /**
+         * given:
+         */
+        val nullString = null
+
+        /**
+         * when:
+         */
+        val actual = Description.new(nullString)
+
+        /**
+         * then:
+         */
+        val expected = Description.ValidationError.Required.invalidNel()
+        assertThat(actual).isEqualTo(expected)
     }
 
     /**
@@ -38,6 +81,8 @@ class DescriptionTest {
     class DescriptionValidRange : ArbitrarySupplier<String> {
         override fun get(): Arbitrary<String> =
             Arbitraries.strings()
+                .ofMinLength(0)
+                .ofMaxLength(64)
                 .filter { !it.startsWith("diff-") }
     }
 }
