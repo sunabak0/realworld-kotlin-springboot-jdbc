c54fbb1948fa6d373479f77881f1c31014c7522d
Msksgm <msksgm3124@gmail.com>
2022-09-07 06:33:00 +0900

    test: 整数の引数によるテストを Property Based Test に変更

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/request/NullableCommentIdTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/request/NullableCommentIdTest.kt
@@ -3,7 +3,6 @@ package com.example.realworldkotlinspringbootjdbc.presentation.request
 import net.jqwik.api.ForAll
 import net.jqwik.api.Property
 import net.jqwik.api.constraints.IntRange
-import net.jqwik.api.constraints.Negative
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -12,13 +11,13 @@ class NullableCommentIdTest {
     @Nested
     class `引数の種類よって戻り値の型を確認する` {
         @Property
-        fun `正常系-0 以上の整数の場合、戻り値は整数`(
-            @ForAll @IntRange(min = 0, max = Int.MAX_VALUE) positiveNumberOrZero: Int
+        fun `正常系-整数の場合、戻り値は整数`(
+            @ForAll @IntRange(min = Int.MIN_VALUE, max = Int.MAX_VALUE) intNumber: Int
         ) {
             /**
              * given:
              */
-            val pathParam = positiveNumberOrZero.toString()
+            val pathParam = intNumber.toString()
 
             /**
              * when:
@@ -28,52 +27,24 @@ class NullableCommentIdTest {
             /**
              * then:
              */
-            Assertions.assertThat(actual).isEqualTo(positiveNumberOrZero)
-        }
-
-        @Test
-        fun `0の場合、戻り値は整数`() {
-            /**
-             * given:
-             */
-            val pathParam = "0"
-
-            /**
-             * when:
-             */
-            val actual = NullableCommentId.from(pathParam)
-
-            /**
-             * then:
-             */
-            val expected = 0
-            Assertions.assertThat(actual).isEqualTo(expected)
-        }
-
-        @Property
-        fun `負の整数の場合、戻り値は整数`(
-            @ForAll @Negative negativeNumber: Int
-        ) {
-            /**
-             * given:
-             */
-            val pathParam = negativeNumber.toString()
-
-            /**
-             * when:
-             */
-            val actual = NullableCommentId.from(pathParam)
-
-            /**
-             * then
-             */
-            Assertions.assertThat(actual).isEqualTo(negativeNumber)
+            Assertions.assertThat(actual).isEqualTo(intNumber)
         }
 
         @Test
         fun `文字列の場合、戻り値は null`() {
-            val pathParam = "hoge"
+            /**
+             * given:
+             */
+            val pathParam = "dummy"
+
+            /**
+             * when:
+             */
             val actual = NullableCommentId.from(pathParam)
+
+            /**
+             * then:
+             */
             val expected = null
             Assertions.assertThat(actual).isEqualTo(expected)
         }
