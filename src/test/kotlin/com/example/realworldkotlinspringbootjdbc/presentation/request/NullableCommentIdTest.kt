b5126d098d2a4b2c30c3f0ef40a1b52eb627ab60
Msksgm <msksgm3124@gmail.com>
2022-09-07 06:20:40 +0900

    test: String.toIntOrNull の sandbox テストを追加し、「小数部が 0 の小数の場合、戻り値はnull」を検証

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/request/NullableCommentIdTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/request/NullableCommentIdTest.kt
@@ -71,30 +71,6 @@ class NullableCommentIdTest {
         }
 
         @Test
-        fun `小数点つきの正の数（0 以外）の場合、戻り値は null`() {
-            val pathParam = "1.0"
-            val actual = NullableCommentId.from(pathParam)
-            val expected = null
-            Assertions.assertThat(actual).isEqualTo(expected)
-        }
-
-        @Test
-        fun `小数点つきの0の場合、戻り値は null`() {
-            val pathParam = "0.0"
-            val actual = NullableCommentId.from(pathParam)
-            val expected = null
-            Assertions.assertThat(actual).isEqualTo(expected)
-        }
-
-        @Test
-        fun `小数点つきの負の整数の場合、戻り値は null`() {
-            val pathParam = "-1.0"
-            val actual = NullableCommentId.from(pathParam)
-            val expected = null
-            Assertions.assertThat(actual).isEqualTo(expected)
-        }
-
-        @Test
         fun `文字列の場合、戻り値は null`() {
             val pathParam = "hoge"
             val actual = NullableCommentId.from(pathParam)
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/sandbox/kotlin/ToIntOrNull.kt
@@ -0,0 +1,70 @@
+package com.example.realworldkotlinspringbootjdbc.sandbox.kotlin
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+/**
+ * String.toIntOrNull() の sandbox テスト
+ *
+ */
+class ToIntOrNull {
+    @Nested
+    class `小数部が 0 の小数の場合、戻り値は null`() {
+        val expected = null
+
+        @Test
+        fun `整数部が正の数の場合`() {
+            /**
+             * given:
+             */
+            val num = "1.0"
+
+            /**
+             * when:
+             */
+            val actual = num.toIntOrNull()
+
+            /**
+             * then
+             */
+            Assertions.assertThat(actual).isEqualTo(expected)
+        }
+
+        @Test
+        fun `整数部が 0 の場合`() {
+            /**
+             * given:
+             */
+            val num = "0.0"
+
+            /**
+             * when:
+             */
+            val actual = num.toIntOrNull()
+
+            /**
+             * then:
+             */
+            Assertions.assertThat(actual).isEqualTo(expected)
+        }
+
+        @Test
+        fun `整数部が負の数の場合`() {
+            /**
+             * given:
+             */
+            val num = "-1.0"
+
+            /**
+             * when:
+             */
+            val actual = num.toIntOrNull()
+
+            /**
+             * then:
+             */
+            Assertions.assertThat(actual).isEqualTo(expected)
+        }
+    }
+}
