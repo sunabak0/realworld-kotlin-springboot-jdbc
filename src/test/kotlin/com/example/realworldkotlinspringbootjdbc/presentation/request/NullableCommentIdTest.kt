bbce9140174a12776ebd630648277e56dcc6311e
Msksgm <msksgm3124@gmail.com>
2022-09-07 07:18:17 +0900

    test: 「正常系-整数の場合、戻り値は整数」の引数を NullableCommentIdValidRange に委譲

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/request/NullableCommentIdTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/request/NullableCommentIdTest.kt
@@ -1,8 +1,11 @@
 package com.example.realworldkotlinspringbootjdbc.presentation.request
 
+import net.jqwik.api.Arbitraries
+import net.jqwik.api.Arbitrary
+import net.jqwik.api.ArbitrarySupplier
 import net.jqwik.api.ForAll
+import net.jqwik.api.From
 import net.jqwik.api.Property
-import net.jqwik.api.constraints.IntRange
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -12,12 +15,12 @@ class NullableCommentIdTest {
     class `引数の種類よって戻り値の型を確認する` {
         @Property
         fun `正常系-整数の場合、戻り値は整数`(
-            @ForAll @IntRange(min = Int.MIN_VALUE, max = Int.MAX_VALUE) intNumber: Int
+            @ForAll @From(supplier = NullableCommentIdValidRange::class) validInt: Int
         ) {
             /**
              * given:
              */
-            val pathParam = intNumber.toString()
+            val pathParam = validInt.toString()
 
             /**
              * when:
@@ -27,7 +30,7 @@ class NullableCommentIdTest {
             /**
              * then:
              */
-            Assertions.assertThat(actual).isEqualTo(intNumber)
+            Assertions.assertThat(actual).isEqualTo(validInt)
         }
 
         @Test
@@ -67,5 +70,14 @@ class NullableCommentIdTest {
             val expected = null
             Assertions.assertThat(actual).isEqualTo(expected)
         }
+    }
+
+    /**
+     * NullableCommentId の有効な範囲の Int プロパティ
+     */
+    class NullableCommentIdValidRange : ArbitrarySupplier<Int> {
+        override fun get(): Arbitrary<Int> =
+            Arbitraries.integers()
+                .between(Int.MIN_VALUE, Int.MAX_VALUE)
     }
 }
