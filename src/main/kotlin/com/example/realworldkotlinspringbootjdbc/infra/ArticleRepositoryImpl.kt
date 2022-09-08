f2a4881b7d34cfecc42d1686397bd44a7cc6b4d3
sunakan <sunakan03@gmail.com>
2022-09-09 08:33:03 +0900

    make fmt

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/infra/ArticleRepositoryImpl.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/infra/ArticleRepositoryImpl.kt
@@ -20,7 +20,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
 import com.example.realworldkotlinspringbootjdbc.domain.article.Body as ArticleBody
 
 @Repository
