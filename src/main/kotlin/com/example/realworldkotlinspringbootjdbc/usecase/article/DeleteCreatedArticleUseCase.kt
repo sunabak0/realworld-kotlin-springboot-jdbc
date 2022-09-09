bb456177438101dad5e0c426e4e377342a03a6c0
sunakan <sunakan03@gmail.com>
2022-09-09 21:46:32 +0900

    【記事削除】UseCaseの実装

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/article/DeleteCreatedArticleUseCase.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/article/DeleteCreatedArticleUseCase.kt
@@ -1,6 +1,11 @@
 package com.example.realworldkotlinspringbootjdbc.usecase.article
 
 import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import arrow.core.left
+import arrow.core.right
+import com.example.realworldkotlinspringbootjdbc.domain.ArticleRepository
 import com.example.realworldkotlinspringbootjdbc.domain.RegisteredUser
 import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
 import com.example.realworldkotlinspringbootjdbc.util.MyError
@@ -17,4 +22,46 @@ interface DeleteCreatedArticleUseCase {
 }
 
 @Service
-class DeleteCreatedArticleUseCaseImpl : DeleteCreatedArticleUseCase
+class DeleteCreatedArticleUseCaseImpl(
+    val articleRepository: ArticleRepository,
+) : DeleteCreatedArticleUseCase {
+    override fun execute(currentUser: RegisteredUser, slug: String?): Either<DeleteCreatedArticleUseCase.Error, Unit> {
+        /**
+         * バリデーション
+         * Invalid -> 早期リターン
+         */
+        val validatedSlug = Slug.new(slug).fold(
+            { return DeleteCreatedArticleUseCase.Error.ValidationError(errors = it).left() },
+            { it }
+        )
+
+        /**
+         * 検索
+         * NotFound -> 早期リターン
+         */
+        val foundArticle = when (val targetArticle = articleRepository.findBySlug(validatedSlug)) {
+            is Left -> when (targetArticle.value) {
+                is ArticleRepository.FindBySlugError.NotFound -> return DeleteCreatedArticleUseCase.Error.NotFoundArticle(
+                    slug = validatedSlug
+                ).left()
+                is ArticleRepository.FindBySlugError.Unexpected -> TODO("想定外なので、TODOにしておく")
+            }
+            is Right -> targetArticle.value
+        }
+
+        /**
+         * 削除
+         */
+        return when (val deleteResult = articleRepository.delete(foundArticle.id)) {
+            is Left -> when (deleteResult.value) {
+                /**
+                 * NotFound
+                 */
+                is ArticleRepository.DeleteError.NotFoundArticle -> DeleteCreatedArticleUseCase.Error.NotFoundArticle(
+                    slug = validatedSlug
+                ).left()
+            }
+            is Right -> Unit.right()
+        }
+    }
+}
