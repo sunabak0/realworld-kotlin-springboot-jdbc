623fb2c802808819a52cde28af10afd77bc8f0d3
sunakan <sunakan03@gmail.com>
2022-09-10 15:29:25 +0900

    【記事削除】UseCaseの修正(著者かどうかを入れた)

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/article/DeleteCreatedArticleUseCase.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/usecase/article/DeleteCreatedArticleUseCase.kt
@@ -3,21 +3,30 @@ package com.example.realworldkotlinspringbootjdbc.usecase.article
 import arrow.core.Either
 import arrow.core.Either.Left
 import arrow.core.Either.Right
+import arrow.core.Invalid
+import arrow.core.Valid
 import arrow.core.left
 import arrow.core.right
 import com.example.realworldkotlinspringbootjdbc.domain.ArticleRepository
+import com.example.realworldkotlinspringbootjdbc.domain.CreatedArticle
+import com.example.realworldkotlinspringbootjdbc.domain.CreatedArticleAuthorValidationDomainService
 import com.example.realworldkotlinspringbootjdbc.domain.RegisteredUser
 import com.example.realworldkotlinspringbootjdbc.domain.article.Slug
 import com.example.realworldkotlinspringbootjdbc.util.MyError
 import org.springframework.stereotype.Service
 
 interface DeleteCreatedArticleUseCase {
-    fun execute(currentUser: RegisteredUser, slug: String?): Either<Error, Unit> = TODO()
+    fun execute(author: RegisteredUser, slug: String?): Either<Error, Unit> = TODO()
 
     sealed interface Error : MyError {
         data class ValidationError(override val errors: List<MyError.ValidationError>) :
             Error, MyError.ValidationErrors
         data class NotFoundArticle(val slug: Slug) : Error, MyError.Basic
+        data class NotAuthor(
+            override val cause: MyError,
+            val targetArticle: CreatedArticle,
+            val notAuthorizedUser: RegisteredUser,
+        ) : Error, MyError.MyErrorWithMyError
     }
 }
 
@@ -25,7 +34,7 @@ interface DeleteCreatedArticleUseCase {
 class DeleteCreatedArticleUseCaseImpl(
     val articleRepository: ArticleRepository,
 ) : DeleteCreatedArticleUseCase {
-    override fun execute(currentUser: RegisteredUser, slug: String?): Either<DeleteCreatedArticleUseCase.Error, Unit> {
+    override fun execute(author: RegisteredUser, slug: String?): Either<DeleteCreatedArticleUseCase.Error, Unit> {
         /**
          * バリデーション
          * Invalid -> 早期リターン
@@ -50,18 +59,42 @@ class DeleteCreatedArticleUseCaseImpl(
         }
 
         /**
-         * 削除
+         * 著者かどうか
+         * 著者ではない -> 早期リターン
+         * 著者である -> 削除
          */
-        return when (val deleteResult = articleRepository.delete(foundArticle.id)) {
-            is Left -> when (deleteResult.value) {
+        return when (
+            val result = CreatedArticleAuthorValidationDomainService.validate(
+                article = foundArticle,
+                user = author
+            )
+        ) {
+            /**
+             * 著者ではない
+             */
+            is Invalid -> DeleteCreatedArticleUseCase.Error.NotAuthor(
+                cause = result.value,
+                targetArticle = foundArticle,
+                notAuthorizedUser = author
+            ).left()
+
+            /**
+             * 著者である -> 削除
+             */
+            is Valid -> when (val deleteResult = articleRepository.delete(foundArticle.id)) {
                 /**
-                 * NotFound
+                 * 削除: 失敗
                  */
-                is ArticleRepository.DeleteError.NotFoundArticle -> DeleteCreatedArticleUseCase.Error.NotFoundArticle(
-                    slug = validatedSlug
-                ).left()
+                is Left -> when (deleteResult.value) {
+                    is ArticleRepository.DeleteError.NotFoundArticle -> DeleteCreatedArticleUseCase.Error.NotFoundArticle(
+                        slug = validatedSlug
+                    ).left()
+                }
+                /**
+                 * 削除: 成功
+                 */
+                is Right -> Unit.right()
             }
-            is Right -> Unit.right()
         }
     }
 }
