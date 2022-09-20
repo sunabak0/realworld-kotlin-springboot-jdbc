0ebeed63b1f1c2edc9bc4c83441ab47b72c90482
sunakan <sunakan03@gmail.com>
2022-09-20 22:10:47 +0900

    delete: RealworldKotlinSpringbootJdbcApplicationTests.kt

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/RealworldKotlinSpringbootJdbcApplicationTests.kt
+++
@@ -1,8 +0,0 @@
-package com.example.realworldkotlinspringbootjdbc
-
-// @SpringBootTest
-// class RealworldKotlinSpringbootJdbcApplicationTests {
-//    @Test
-//    fun contextLoads() {
-//    }
-// }
