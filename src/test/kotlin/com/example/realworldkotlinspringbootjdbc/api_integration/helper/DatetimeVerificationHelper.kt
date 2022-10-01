66ce7ef3e5b51b05ec397627d7baeaece6081bf1
Msksgm <msksgm3124@gmail.com>
2022-10-02 07:46:06 +0900

    test: ISO8601 かつ UTC なのか確認するバリデーション関数 ExpectIso8601UtcAndParsable を追加

---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/helper/DatetimeVerificationHelper.kt
@@ -0,0 +1,13 @@
+package com.example.realworldkotlinspringbootjdbc.api_integration.helper
+
+import org.assertj.core.api.Assertions.assertThat
+import java.time.OffsetDateTime
+
+object DatetimeVerificationHelper {
+    fun expectIso8601UtcAndParsable(o: Any): Boolean {
+        assertThat(o.toString())
+            .`as`("YYYY-MM-DDTHH:mm:ss.SSSXXX(ISO8601形式)で、TimeZoneはUTCである")
+            .matches("([0-9]{4})-([0-9]{2})-([0-9]{2}T([0-9]{2}):([0-9]{2}):([0-9]{2}).([0-9]{3})Z)")
+        return runCatching { OffsetDateTime.parse(o.toString()) }.isSuccess
+    }
+}
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/helper/DatetimeVerificationHelperTest.kt
@@ -0,0 +1,36 @@
+package com.example.realworldkotlinspringbootjdbc.api_integration.helper
+
+import net.jqwik.api.ForAll
+import net.jqwik.api.Property
+import net.jqwik.time.api.constraints.OffsetRange
+import org.assertj.core.api.Assertions.assertThat
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+class DatetimeVerificationHelperTest {
+    class ExpectIso8601UtcAndParsable {
+        @Property
+        fun `正常系-ISO8601 かつ UTC の場合 true が戻り値`(
+            @ForAll @OffsetRange(
+                min = "Z",
+                max = "Z"
+            ) offsetDateTime: OffsetDateTime
+        ) {
+            /**
+             * given:
+             * - ISO8601 にフォーマット
+             */
+            val formatted = offsetDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"))
+
+            /**
+             * when:
+             */
+            val actual = DatetimeVerificationHelper.expectIso8601UtcAndParsable(formatted)
+
+            /**
+             * then:
+             */
+            assertThat(actual).isTrue
+        }
+    }
+}
