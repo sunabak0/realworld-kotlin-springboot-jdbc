b1197e754db866c78c1a86f1bce97667a8861211
sunakan <sunakan03@gmail.com>
2022-10-10 15:38:35 +0900

    アノテーションを使ったリクエストボディのバリデーションでNGだった場合のエラーハンドリングを明示化
    __背景__
    
    - 暗黙的に処理されており、分かりづらかった
    
    __対応__
    
    - 明示化

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/shared/SharedRealworldExceptionHandlers.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/shared/SharedRealworldExceptionHandlers.kt
@@ -2,8 +2,12 @@ package com.example.realworldkotlinspringbootjdbc.presentation.shared
 
 import com.example.realworldkotlinspringbootjdbc.openapi.generated.model.GenericErrorModel
 import com.example.realworldkotlinspringbootjdbc.openapi.generated.model.GenericErrorModelErrors
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -24,6 +28,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
  */
 @RestControllerAdvice
 class SharedRealworldExceptionHandlers {
+    /**
+     * JSONのパースやMappingに失敗した場合のエラーハンドリング
+     *
+     * - リクエストボディが{ "foo": "hello }や""等、JSONのパースに失敗する場合、ここでエラーハンドリングされる
+     * - リクエストボディがrequire = true等のフィールドを満たしていない場合、ここでエラーハンドリングされる
+     *   - 例: @field:JsonProperty("user", required = true) val user: String
+     *
+     * 参考
+     * - [Additional Handling of Jackson Parsing and Mapping Errors](https://dougbreaux.github.io/2020/10/11/Additional-Jackson-Parse-Errors.html)
+     */
+    @ExceptionHandler(value = [HttpMessageNotReadableException::class])
+    fun onBindException(e: HttpMessageNotReadableException): ResponseEntity<GenericErrorModel> =
+        when (e.cause) {
+            is JsonParseException -> ResponseEntity(
+                GenericErrorModel(GenericErrorModelErrors(body = listOf("リクエストボディが読み取れませんでした(Jsonとして間違っている可能性があります)"))),
+                HttpStatus.valueOf(400)
+            )
+
+            is MismatchedInputException -> ResponseEntity(
+                GenericErrorModel(GenericErrorModelErrors(body = listOf("リクエストボディが読み取れませんでした(要求しているJSONの形ではない可能性があります)"))),
+                HttpStatus.valueOf(400)
+            )
+
+            is JsonMappingException -> ResponseEntity(
+                GenericErrorModel(GenericErrorModelErrors(body = listOf("リクエストボディが読み取れませんでした(マッピングに失敗した可能性があります)"))),
+                HttpStatus.valueOf(400)
+            )
+
+            else -> ResponseEntity(
+                GenericErrorModel(GenericErrorModelErrors(body = listOf("リクエストボディが読み取れませんでした"))),
+                HttpStatus.valueOf(400)
+            )
+        }
+
     /**
      * セッションのエンコード時に失敗した時のエラーハンドリング
      *
--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/shared/SharedRealworldExceptionHandlersTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/shared/SharedRealworldExceptionHandlersTest.kt
@@ -6,6 +6,7 @@ import com.example.realworldkotlinspringbootjdbc.domain.user.UserId
 import com.example.realworldkotlinspringbootjdbc.presentation.shared.RealworldSessionEncodeErrorException
 import com.example.realworldkotlinspringbootjdbc.util.MySession
 import com.example.realworldkotlinspringbootjdbc.util.MySessionJwt
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.skyscreamer.jsonassert.JSONAssert
@@ -14,11 +15,16 @@ import org.skyscreamer.jsonassert.comparator.CustomComparator
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 class SharedRealworldExceptionController {
@@ -35,6 +41,18 @@ class SharedRealworldExceptionController {
                 )
             )
         )
+
+    data class DummyRequestBody(
+        @field:Valid
+        @field:JsonProperty("name", required = true) val name: String,
+        @field:Valid
+        @field:JsonProperty("age", required = true) val age: Int
+    )
+    @PostMapping("/throw_http_message_not_readable_exception")
+    fun throwHttpMessageNotReadableException(
+        @RequestBody @Valid requestBody: DummyRequestBody
+    ): ResponseEntity<String> =
+        throw UnsupportedOperationException("これがthrowされる前にHttpMessageNotReadableExceptionがthrowされる: $requestBody")
 }
 
 @SpringBootTest
@@ -43,6 +61,49 @@ class SharedRealworldExceptionHandlersTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `異常系-リクエストボディをclassにマッピングするまでに異常があった場合、400エラーレスポンスを返す`() {
+        /**
+         * given:
+         * - DummyRequestBodyが要求している形にそぐわないJSON
+         */
+        val wrongRequestBody = """
+            {
+                "foo": "bar"
+            }
+        """.trimIndent()
+
+        /**
+         * when:
+         */
+        val response = mockMvc.perform(
+            MockMvcRequestBuilders
+                .post("/throw_http_message_not_readable_exception")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(wrongRequestBody)
+        ).andReturn().response
+        val actualStatus = response.status
+        val actualResponseBody = response.contentAsString
+
+        /**
+         * then:
+         */
+        val expectedStatus = 400
+        val expectedResponseBody = """
+            {
+              "errors": {
+                "body": ["リクエストボディが読み取れませんでした(要求しているJSONの形ではない可能性があります)"]
+              }
+            }
+        """.trimIndent()
+        assertThat(actualStatus).isEqualTo(expectedStatus)
+        JSONAssert.assertEquals(
+            expectedResponseBody,
+            actualResponseBody,
+            CustomComparator(JSONCompareMode.NON_EXTENSIBLE)
+        )
+    }
 
     @Test
     fun `異常系-セッションエンコードに失敗したという旨の例外が投げられた時、500エラーレスポンスを返す`() {
