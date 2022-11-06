4c20d96ff103a094d4761795a8a082a6b39fd9c1
Msksgm <msksgm3124@gmail.com>
2022-11-06 14:05:53 +0900

    test: テスト「正常系-ログイン状態で、slug で指定した作成済記事のコメントが存在する場合、コメントが全て取得され、author がフォロイーの場合、following キーのバリューが true になる」を追加
    get /api/articles/${slug}/comments のテスト

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/CommentTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/CommentTest.kt
@@ -50,9 +50,10 @@ class CommentTest {
                 "datasets/yml/given/users.yml"
             ]
         )
-        fun `正常系-slug で指定した作成済記事のコメントが存在する場合全て取得される`() {
+        fun `正常系-未ログインで、slug で指定した作成済記事のコメントが存在する場合全て取得される`() {
             /**
              * given:
+             * - 作成済記事が存在する slug
              */
             val slug = "rust-vs-scala-vs-kotlin"
 
@@ -99,6 +100,101 @@ class CommentTest {
                             "bio": "Rubyを作った",
                             image: "",
                             following: false
+                          }
+                        },
+                        {
+                          "id": 5,
+                          "body": "dummy-comment-body-02",
+                          "createdAt": "2022-01-01T00:00:00.000Z",
+                          "updatedAt": "2022-01-01T00:00:00.000Z",
+                          "author": {
+                            "username": "graydon-hoare",
+                            "bio": "Rustを作った",
+                            image: "",
+                            following: false
+                          }
+                        }
+                      ]
+                    }
+                """.trimIndent()
+            assertThat(actualStatus).isEqualTo(expectedStatus)
+            JSONAssert.assertEquals(
+                expectedResponseBody,
+                actualResponseBody,
+                CustomComparator(
+                    JSONCompareMode.STRICT,
+                    Customization("*.createdAt") { actualCreatedAt, expectedDummy ->
+                        DatetimeVerificationHelper.expectIso8601UtcWithoutMillisecondAndParsable(actualCreatedAt) && expectedDummy == "2022-01-01T00:00:00.000Z"
+                    },
+                    Customization("*.updatedAt") { actualUpdatedAt, expectedDummy ->
+                        DatetimeVerificationHelper.expectIso8601UtcWithoutMillisecondAndParsable(actualUpdatedAt) && expectedDummy == "2022-01-01T00:00:00.000Z"
+                    },
+                )
+            )
+        }
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/articles.yml",
+                "datasets/yml/given/users.yml"
+            ]
+        )
+        fun `正常系-ログイン状態で、slug で指定した作成済記事のコメントが存在する場合、コメントが全て取得され、author がフォロイーの場合、following キーのバリューが true になる`() {
+            /**
+             * given:
+             * - フォロイーが存在するユーザー（userId = 3）の sessionToken
+             * - 作成済記事が存在する slug
+             */
+            val existedUser = SeedData.users().filter { it.userId.value == 3 }[0]
+            val sessionToken = MySessionJwtImpl.encode(MySession(existedUser.userId, existedUser.email))
+                .getOrHandle { throw UnsupportedOperationException("セッションからJWTへの変換に失敗しました(前提条件であるため、元の実装を見直してください)") }
+            val slug = "rust-vs-scala-vs-kotlin"
+
+            /**
+             * when:
+             */
+            val response = mockMvc.perform(
+                MockMvcRequestBuilders
+                    .get("/api/articles/$slug/comments")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", sessionToken)
+            ).andReturn().response
+            val actualStatus = response.status
+            val actualResponseBody = response.contentAsString
+
+            /**
+             * then:
+             * - ステータスコードが一致する
+             * - レスポンスボディが一致する
+             */
+            val expectedStatus = HttpStatus.OK.value()
+            val expectedResponseBody =
+                """
+                    {
+                      "comments": [
+                        {
+                          "id": 1,
+                          "body": "dummy-comment-body-01",
+                          "createdAt": "2022-01-01T00:00:00.000Z",
+                          "updatedAt": "2022-01-01T00:00:00.000Z",
+                          "author": {
+                            "username": "graydon-hoare",
+                            "bio": "Rustを作った",
+                            image: "",
+                            following: false
+                          }
+                        },
+                        {
+                          "id": 3,
+                          "body": "dummy-comment-body-03",
+                          "createdAt": "2022-01-01T00:00:00.000Z",
+                          "updatedAt": "2022-01-01T00:00:00.000Z",
+                          "author": {
+                            "username": "松本行弘",
+                            "bio": "Rubyを作った",
+                            image: "",
+                            following: true
                           }
                         },
                         {
