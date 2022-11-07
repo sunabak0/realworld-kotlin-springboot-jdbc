3289854060839c25cb8b32a3726119d726b27512
Msksgm <msksgm3124@gmail.com>
2022-11-07 09:20:01 +0900

    test: 「正常系-username で指定したユーザーが存在し、フォロー済の場合、Profile が戻り値で following=true」テストを追加
    フォロイーかどうかに関わらず、following=true であることを確認するため

--- src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/ProfileTest.kt
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/ProfileTest.kt
@@ -325,5 +325,59 @@ class ProfileTest {
                 JSONCompareMode.STRICT
             )
         }
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/users.yml"
+            ]
+        )
+        fun `正常系-username で指定したユーザーが存在し、フォロー済の場合、Profile が戻り値で following=true`() {
+            /**
+             * given:
+             * - 登録済ユーザーが存在する username
+             * - username がフォロイーである、登録済ユーザーの sessionToken
+             */
+            val username = "paul-graham"
+            val existedUser = SeedData.users().filter { it.userId.value == 3 }[0]
+            val sessionToken = MySessionJwtImpl.encode(MySession(existedUser.userId, existedUser.email))
+                .getOrHandle { throw UnsupportedOperationException("セッションからJWTへの変換に失敗しました(前提条件であるため、元の実装を見直してください)") }
+
+            /**
+             * when:
+             */
+            val response = mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/api/profiles/$username/follow")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", sessionToken)
+            ).andReturn().response
+            val actualStatus = response.status
+            val actualResponseBody = response.contentAsString
+
+            /**
+             * then:
+             * - ステータスコードが一致する
+             * - レスポンスボディが一致する
+             */
+            val expectedStatus = HttpStatus.OK.value()
+            val expectedResponseBody =
+                """
+                    {
+                      "profile": {
+                        "username": "paul-graham",
+                        "bio": "Lisper",
+                        "image": "",
+                        "following": true
+                      }
+                    }
+                """.trimIndent()
+            assertThat(actualStatus).isEqualTo(expectedStatus)
+            JSONAssert.assertEquals(
+                expectedResponseBody,
+                actualResponseBody,
+                JSONCompareMode.STRICT
+            )
+        }
     }
 }
