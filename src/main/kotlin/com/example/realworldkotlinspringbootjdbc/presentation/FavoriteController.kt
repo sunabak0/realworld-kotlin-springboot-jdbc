4e8e9795d33289cee867ddd8cfaa52debb8b03a6
Msksgm <msksgm3124@gmail.com>
2022-11-12 09:29:45 +0900

    feat: OpenAPI Generator から生成した、post /articles/{slug}/favorite の正常系を実装。author レスポンスの中身は、一時的に dummy に置き換え

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/FavoriteController.kt
+++ src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/FavoriteController.kt
@@ -2,11 +2,16 @@ package com.example.realworldkotlinspringbootjdbc.presentation
 
 import arrow.core.Either.Left
 import arrow.core.Either.Right
+import com.example.realworldkotlinspringbootjdbc.openapi.generated.controller.FavoritesApi
+import com.example.realworldkotlinspringbootjdbc.openapi.generated.model.Profile
+import com.example.realworldkotlinspringbootjdbc.openapi.generated.model.SingleArticleResponse
 import com.example.realworldkotlinspringbootjdbc.presentation.response.Article
 import com.example.realworldkotlinspringbootjdbc.presentation.response.serializeUnexpectedErrorForResponseBody
 import com.example.realworldkotlinspringbootjdbc.presentation.shared.AuthorizationError
+import com.example.realworldkotlinspringbootjdbc.presentation.shared.RealworldAuthenticationUseCaseUnauthorizedException
 import com.example.realworldkotlinspringbootjdbc.usecase.favorite.FavoriteUseCase
 import com.example.realworldkotlinspringbootjdbc.usecase.favorite.UnfavoriteUseCase
+import com.example.realworldkotlinspringbootjdbc.usecase.shared.RealworldAuthenticationUseCase
 import com.example.realworldkotlinspringbootjdbc.util.MyAuth
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
@@ -14,77 +19,115 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 import java.text.SimpleDateFormat
+import java.time.ZoneOffset
 
 @RestController
 class FavoriteController(
     val myAuth: MyAuth,
+    val realworldAuthenticationUseCase: RealworldAuthenticationUseCase,
     val favoriteUseCase: FavoriteUseCase,
     val unfavoriteUseCase: UnfavoriteUseCase,
-) {
-    @PostMapping("/articles/{slug}/favorite")
-    fun favorite(
-        @RequestHeader("Authorization") rawAuthorizationHeader: String?,
-        @PathVariable("slug") slug: String?
-    ): ResponseEntity<String> {
-        return when (val authorizeResult = myAuth.authorize(rawAuthorizationHeader)) {
-            /**
-             * JWT 認証 失敗
-             */
-            is Left -> AuthorizationError.handle()
-            /**
-             * JWT 認証 成功
-             */
-            is Right -> {
-                when (val favoritedArticle = favoriteUseCase.execute(slug, authorizeResult.value)) {
-                    /**
-                     * お気に入り登録 失敗
-                     */
-                    is Left -> when (favoritedArticle.value) {
-                        /**
-                         * 原因: Slug がバリデーションエラー
-                         */
-                        is FavoriteUseCase.Error.InvalidSlug -> ResponseEntity(
-                            serializeUnexpectedErrorForResponseBody("記事が見つかりませんでした"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
-                            HttpStatus.valueOf(404)
-                        )
-                        /**
-                         * 原因: 記事が見つからなかった
-                         */
-                        is FavoriteUseCase.Error.NotFoundArticleBySlug -> ResponseEntity(
-                            serializeUnexpectedErrorForResponseBody("記事が見つかりませんでした"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
-                            HttpStatus.valueOf(404)
-                        )
-                    }
-                    /**
-                     * お気に入り登録 成功
-                     */
-                    is Right -> {
-                        val article = Article(
-                            favoritedArticle.value.title.value,
-                            favoritedArticle.value.slug.value,
-                            favoritedArticle.value.body.value,
-                            favoritedArticle.value.createdAt,
-                            favoritedArticle.value.updatedAt,
-                            favoritedArticle.value.description.value,
-                            favoritedArticle.value.tagList.map { tag -> tag.value },
-                            // TODO: authorId を author に変更
-                            favoritedArticle.value.authorId.value,
-                            favoritedArticle.value.favorited,
-                            favoritedArticle.value.favoritesCount
-                        )
-                        return ResponseEntity(
-                            ObjectMapper().enable(SerializationFeature.WRAP_ROOT_VALUE).writeValueAsString(article),
-                            HttpStatus.valueOf(200),
-                        )
-                    }
-                }
-            }
-        }
+) : FavoritesApi {
+    override fun createArticleFavorite(authorization: String, slug: String): ResponseEntity<SingleArticleResponse> {
+        val currentUser = realworldAuthenticationUseCase.execute(authorization).fold(
+            { throw RealworldAuthenticationUseCaseUnauthorizedException(it) },
+            { it }
+        )
+
+        val favoritedArticle = favoriteUseCase.execute(slug, currentUser).fold(
+            { throw TODO() },
+            { it }
+        )
+
+        return ResponseEntity(
+            SingleArticleResponse(
+                com.example.realworldkotlinspringbootjdbc.openapi.generated.model.Article(
+                    slug = favoritedArticle.slug.value,
+                    title = favoritedArticle.title.value,
+                    description = favoritedArticle.description.value,
+                    body = favoritedArticle.body.value,
+                    tagList = favoritedArticle.tagList.map { it.value },
+                    createdAt = favoritedArticle.createdAt.toInstant().atOffset(ZoneOffset.UTC),
+                    updatedAt = favoritedArticle.updatedAt.toInstant().atOffset(ZoneOffset.UTC),
+                    favorited = favoritedArticle.favorited,
+                    favoritesCount = favoritedArticle.favoritesCount,
+                    author = Profile(
+                        username = "dummy-username",
+                        bio = "dummy-bio",
+                        image = "dummy-image",
+                        following = false
+                    )
+                )
+            ),
+            HttpStatus.OK
+        )
     }
+    // TODO: 以下の項目を実装したら削除
+    //  - CreatedArticleWithAuthor を取得するクエリモデルが実装
+    //  - 準正常系を実装
+    // @PostMapping("/articles/{slug}/favorite")
+    // fun favorite(
+    //     @RequestHeader("Authorization") rawAuthorizationHeader: String?,
+    //     @PathVariable("slug") slug: String?
+    // ): ResponseEntity<String> {
+    //     return when (val authorizeResult = myAuth.authorize(rawAuthorizationHeader)) {
+    //         /**
+    //          * JWT 認証 失敗
+    //          */
+    //         is Left -> AuthorizationError.handle()
+    //         /**
+    //          * JWT 認証 成功
+    //          */
+    //         is Right -> {
+    //             when (val favoritedArticle = favoriteUseCase.execute(slug, authorizeResult.value)) {
+    //                 /**
+    //                  * お気に入り登録 失敗
+    //                  */
+    //                 is Left -> when (favoritedArticle.value) {
+    //                     /**
+    //                      * 原因: Slug がバリデーションエラー
+    //                      */
+    //                     is FavoriteUseCase.Error.InvalidSlug -> ResponseEntity(
+    //                         serializeUnexpectedErrorForResponseBody("記事が見つかりませんでした"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
+    //                         HttpStatus.valueOf(404)
+    //                     )
+    //                     /**
+    //                      * 原因: 記事が見つからなかった
+    //                      */
+    //                     is FavoriteUseCase.Error.NotFoundArticleBySlug -> ResponseEntity(
+    //                         serializeUnexpectedErrorForResponseBody("記事が見つかりませんでした"), // TODO: serializeUnexpectedErrorForResponseBodyをやめる
+    //                         HttpStatus.valueOf(404)
+    //                     )
+    //                 }
+    //                 /**
+    //                  * お気に入り登録 成功
+    //                  */
+    //                 is Right -> {
+    //                     val article = Article(
+    //                         favoritedArticle.value.title.value,
+    //                         favoritedArticle.value.slug.value,
+    //                         favoritedArticle.value.body.value,
+    //                         favoritedArticle.value.createdAt,
+    //                         favoritedArticle.value.updatedAt,
+    //                         favoritedArticle.value.description.value,
+    //                         favoritedArticle.value.tagList.map { tag -> tag.value },
+    //                         // TODO: authorId を author に変更
+    //                         favoritedArticle.value.authorId.value,
+    //                         favoritedArticle.value.favorited,
+    //                         favoritedArticle.value.favoritesCount
+    //                     )
+    //                     return ResponseEntity(
+    //                         ObjectMapper().enable(SerializationFeature.WRAP_ROOT_VALUE).writeValueAsString(article),
+    //                         HttpStatus.valueOf(200),
+    //                     )
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
     @DeleteMapping("/articles/{slug}/favorite")
     fun unfavorite(
---
+++ src/test/kotlin/com/example/realworldkotlinspringbootjdbc/api_integration/FavoriteTest.kt
@@ -0,0 +1,126 @@
+package com.example.realworldkotlinspringbootjdbc.api_integration
+
+import arrow.core.getOrHandle
+import com.example.realworldkotlinspringbootjdbc.api_integration.helper.DatetimeVerificationHelper
+import com.example.realworldkotlinspringbootjdbc.infra.DbConnection
+import com.example.realworldkotlinspringbootjdbc.infra.helper.SeedData
+import com.example.realworldkotlinspringbootjdbc.util.MySession
+import com.example.realworldkotlinspringbootjdbc.util.MySessionJwtImpl
+import com.github.database.rider.core.api.dataset.DataSet
+import com.github.database.rider.junit5.api.DBRider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.skyscreamer.jsonassert.Customization
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
+import org.skyscreamer.jsonassert.comparator.CustomComparator
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+
+class FavoriteTest {
+    @SpringBootTest
+    @AutoConfigureMockMvc
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @DBRider
+    @Tag("ApiIntegration")
+    class CreateArticleFavorite {
+
+        @Autowired
+        lateinit var mockMvc: MockMvc
+
+        @BeforeAll
+        fun reset() = DbConnection.resetSequence()
+
+        @Test
+        @DataSet(
+            value = [
+                "datasets/yml/given/articles.yml",
+                "datasets/yml/given/users.yml",
+                "datasets/yml/given/tags.yml",
+            ]
+        )
+        @ExpectedDataSet(
+            value = ["datasets/yml/then/article_repository/favorite-success.yml"],
+            ignoreCols = ["id", "created_at", "updated_at"],
+            orderBy = ["id"]
+        )
+        fun `正常系-お気に入り未登録の作成済記事の slug を指定した場合、favorited=trueの作成済記事が戻り値`() {
+            /**
+             * given:
+             * - 作成済記事が存在する slug
+             * - 作成済記事をお気に入り未登録のユーザー（userId = 3）の sessionToken
+             */
+            val slug = "rust-vs-scala-vs-kotlin"
+            val existedUser = SeedData.users().filter { it.userId.value == 3 }[0]
+            val sessionToken = MySessionJwtImpl.encode(MySession(existedUser.userId, existedUser.email))
+                .getOrHandle { throw UnsupportedOperationException("セッションからJWTへの変換に失敗しました(前提条件であるため、元の実装を見直してください)") }
+
+            /**
+             * when:
+             */
+            val response = mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/api/articles/$slug/favorite")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", sessionToken)
+            ).andReturn().response
+            val actualStatus = response.status
+            val actualResponseBody = response.contentAsString
+
+            /**
+             * then:
+             * - ステータスコードが一致する
+             * - レスポンスボディが一致する
+             */
+            val expectedStatus = HttpStatus.OK.value()
+            val expectedResponseBody = """
+                {
+                  "article":
+                     {
+                        "title":"Rust vs Scala vs Kotlin",
+                        "slug":"rust-vs-scala-vs-kotlin",
+                        "body":"dummy-body",
+                        "createdAt": "2022-01-01T00:00:00.000Z",
+                        "updatedAt": "2022-01-01T00:00:00.000Z",
+                        "description":"dummy-description",
+                        "tagList": [
+                            "rust",
+                            "scala",
+                            "kotlin"
+                        ],
+                        "favorited":true,
+                        "favoritesCount":2,
+                        "author": {
+                            username: "dummy-username",
+                            bio: "dummy-bio",
+                            image: "dummy-image",
+                            following: false
+                        }
+                     }
+                }
+            """.trimIndent()
+            assertThat(actualStatus).isEqualTo(expectedStatus)
+            JSONAssert.assertEquals(
+                expectedResponseBody,
+                actualResponseBody,
+                CustomComparator(
+                    JSONCompareMode.STRICT,
+                    Customization("*.createdAt") { actualCreatedAt, expectedDummy ->
+                        DatetimeVerificationHelper.expectIso8601UtcWithoutMillisecondAndParsable(actualCreatedAt) && expectedDummy == "2022-01-01T00:00:00.000Z"
+                    },
+                    Customization("*.updatedAt") { actualUpdatedAt, expectedDummy ->
+                        DatetimeVerificationHelper.expectIso8601UtcWithoutMillisecondAndParsable(actualUpdatedAt) && expectedDummy == "2022-01-01T00:00:00.000Z"
+                    },
+                )
+            )
+        }
+    }
+}
