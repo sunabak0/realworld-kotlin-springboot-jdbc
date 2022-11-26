dd4a81c96820f2e60e3c11d8862058787c265584
Msksgm <msksgm3124@gmail.com>
2022-11-26 09:07:55 +0900

    refactor: 認証処理を RealworldAuthenticationUseCase に移行したので MyAuth と AuthorizationError 削除

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/shared/AuthorizationError.kt
+++
@@ -1,8 +0,0 @@
-package com.example.realworldkotlinspringbootjdbc.presentation.shared
-
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-
-object AuthorizationError {
-    fun handle(): ResponseEntity<String> = ResponseEntity("", HttpStatus.valueOf(401))
-}
