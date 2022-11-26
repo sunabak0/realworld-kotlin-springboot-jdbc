54ca2e421690a553c60e08662cf3828d5023901e
Msksgm <49427230+Msksgm@users.noreply.github.com>
2022-11-26 17:10:53 +0900

    どこからも参照されないファイルの削除 (#328)
    * refactor: OpenAPI Generator の導入により、presentation 層の request と response が不要なったので削除
    
    * refactor: 認証処理を RealworldAuthenticationUseCase に移行したので MyAuth と AuthorizationError 削除

--- src/main/kotlin/com/example/realworldkotlinspringbootjdbc/presentation/response/Tags.kt
+++
@@ -1,7 +0,0 @@
-package com.example.realworldkotlinspringbootjdbc.presentation.response
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Tags(
-    @JsonProperty("tags") val tags: List<String>,
-)
